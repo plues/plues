87d3fed8316a9846f6f75dae17da712061db83c7
David Schneider <david.schneider@bivab.de>
2016-08-01 17:04:36 +0200

    Moved properties to an module

--- src/main/java/de/hhu/stups/plues/injector/PluesModule.java
+++ src/main/java/de/hhu/stups/plues/injector/PluesModule.java
@@ -15,50 +15,15 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXMLLoader;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import static java.lang.Thread.currentThread;
-
 public class PluesModule extends AbstractModule {
-    private static Properties setupProperties() {
-        Properties defaults = new Properties();
-        Properties properties = loadProperties(defaults, "main", "local");
-
-        // settings that can be overriden in env vars
-        String[] env = new String[]{"MODELPATH", "DBPATH"};
-
-        for (String it : env) {
-            if (System.getenv(it) != null) {
-                properties.setProperty(it.toLowerCase(), System.getenv(it));
-            }
-        }
-        return properties;
-    }
-
-    private static Properties loadProperties(Properties properties, String... propertyFiles) {
-        for (String propertyFile : propertyFiles) {
-            try {
-                InputStream p = currentThread().getContextClassLoader().getResourceAsStream(propertyFile + ".properties");
-                if (p == null) {
-                    continue;
-                }
-                properties.load(p);
-            } catch (FileNotFoundException e) {
-                System.err.println(propertyFile + ".properties is missing!");
-            } catch (IOException e) {
-                System.err.println(propertyFile + ".properties produced IO Error!");
-            }
-        }
-        return properties;
-    }
 
     @Override
     public void configure() {
         // prob 2.0
         install(new MainModule());
+
+        install(new PropertiesModule());
+
         install(new FactoryModuleBuilder()
                 .build(SolverLoaderTaskFactory.class));
 
@@ -80,8 +45,4 @@ public class PluesModule extends AbstractModule {
         return fxmlLoader;
     }
 
-    @Provides
-    public Properties provideProperties() {
-        return setupProperties();
-    }
 }
---
+++ src/main/java/de/hhu/stups/plues/injector/PropertiesModule.java
@@ -0,0 +1,51 @@
+package de.hhu.stups.plues.injector;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static java.lang.Thread.currentThread;
+
+public class PropertiesModule extends AbstractModule {
+    static Properties setupProperties() {
+        Properties defaults = new Properties();
+        Properties properties = loadProperties(defaults, "main", "local");
+
+        // settings that can be overriden in env vars
+        String[] env = new String[]{"MODELPATH", "DBPATH"};
+
+        for (String it : env) {
+            if (System.getenv(it) != null) {
+                properties.setProperty(it.toLowerCase(), System.getenv(it));
+            }
+        }
+        return properties;
+    }
+
+    private static Properties loadProperties(Properties properties, String... propertyFiles) {
+        for (String propertyFile : propertyFiles) {
+            try {
+                InputStream p = currentThread().getContextClassLoader().getResourceAsStream(propertyFile + ".properties");
+                if (p == null) {
+                    continue;
+                }
+                properties.load(p);
+            } catch (FileNotFoundException e) {
+                System.err.println(propertyFile + ".properties is missing!");
+            } catch (IOException e) {
+                System.err.println(propertyFile + ".properties produced IO Error!");
+            }
+        }
+        return properties;
+    }
+
+
+    @Override
+    public void configure() {
+        bind(Properties.class).toInstance(setupProperties());
+    }
+}
