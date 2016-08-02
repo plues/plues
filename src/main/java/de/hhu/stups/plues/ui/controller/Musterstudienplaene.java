f628852b010236732a46187a55263eb26a589d26
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-08-02 13:48:09 +0200

    Add tab pane to main view and a second tab for musterstudienplaene

---
+++ src/main/java/de/hhu/stups/plues/ui/controller/Musterstudienplaene.java
@@ -0,0 +1,15 @@
+package de.hhu.stups.plues.ui.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Musterstudienplaene extends HBox implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
