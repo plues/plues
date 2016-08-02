7906d978ba02d7abbac26a665beda52b7e886f7d
schmidt <joshua.schmidt@uni-duesseldorf.de>
2016-08-02 16:12:26 +0200

    started implementation of 'Musterstudienplaene' ui and controller

--- src/main/java/de/hhu/stups/plues/ui/controller/Musterstudienplaene.java
+++ src/main/java/de/hhu/stups/plues/ui/controller/Musterstudienplaene.java
@@ -2,30 +2,31 @@ package de.hhu.stups.plues.ui.controller;
 
 import com.google.inject.Inject;
 import de.hhu.stups.plues.data.AbstractStore;
+import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.prob.Solver;
 import javafx.beans.property.ObjectProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import static tlc2.util.SimpUtil.store;
-
-public class Musterstudienplaene extends VBox implements Initializable {
+public class Musterstudienplaene extends GridPane implements Initializable {
 
     private final ObjectProperty<AbstractStore> storeProperty;
     private final ObjectProperty<Solver> solverProperty;
 
     @FXML
-    ComboBox major;
-
+    ComboBox<Course> cbMajor;
     @FXML
-    ComboBox minor;
+    ComboBox<Course> cbMinor;
 
     @Inject
     public Musterstudienplaene(FXMLLoader loader, ObjectProperty<AbstractStore> storeProperty, ObjectProperty<Solver> solverProperty) {
@@ -42,11 +43,39 @@ public class Musterstudienplaene extends VBox implements Initializable {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-        System.out.println("Store" + store);
+    }
+
+    @FXML
+    public void btGeneratePressed(){
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        AbstractStore store = storeProperty.get();
+
+        if (store == null){
+            storeProperty.addListener((observable, oldValue, newValue) -> {
+                    this.initializeComboBoxes(newValue);});
+        } else {
+            this.initializeComboBoxes(store);
+        }
     }
+
+    private void initializeComboBoxes(AbstractStore store){
+        List<Course> courses = store.getCourses();
+        List<Course> majorCourses = new ArrayList<>();
+        List<Course> minorCourses = new ArrayList<>();
+        for (Course c : courses){
+            String kzfa = c.getKzfa();
+            if (kzfa.equals("H")){
+                majorCourses.add(c);
+            } else {
+                minorCourses.add(c);
+            }
+        }
+        cbMajor.setItems((FXCollections.observableArrayList(majorCourses)));
+        cbMinor.setItems((FXCollections.observableArrayList(minorCourses)));
+    }
+
 }
