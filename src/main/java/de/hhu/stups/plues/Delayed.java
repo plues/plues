00152a34d35b31dc5d53c16bd9e8be861c03e001
David Schneider <david.schneider@bivab.de>
2016-08-03 09:42:14 +0200

    Made set and whenAvailable methods on Delayed synchronised

--- src/main/java/de/hhu/stups/plues/Delayed.java
+++ src/main/java/de/hhu/stups/plues/Delayed.java
@@ -4,6 +4,14 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 
+
+/**
+ * Delayed<T> represents an instance of T that becomes available with an arbitrary delay.
+ * Consumers can register Event handlers to be executed once the T object is available.
+ * If the T object is already set, new event handlers are executed immediately.
+ *
+ * @param <T>
+ */
 public class Delayed<T> {
     private T instance;
     private List<Consumer<T>> listeners;
@@ -12,7 +20,7 @@ public class Delayed<T> {
         this.listeners = new LinkedList<>();
     }
 
-    public void whenAvailable(Consumer<T> r) {
+    public synchronized void whenAvailable(Consumer<T> r) {
         if (this.instance != null) {
             r.accept(this.instance);
         } else {
@@ -20,7 +28,7 @@ public class Delayed<T> {
         }
     }
 
-    public void set(T instance) {
+    public synchronized void set(T instance) {
         this.instance = instance;
         for (Consumer<T> r : this.listeners) {
             r.accept(this.instance);
