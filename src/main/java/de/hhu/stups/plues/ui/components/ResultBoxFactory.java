0c75002fe6de7b7b3a4fda8046b00fb7ae51d515
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-08-15 11:49:04 +0200

    Change style to match check style convention

--- src/main/java/de/hhu/stups/plues/ui/components/ResultBoxFactory.java
+++ src/main/java/de/hhu/stups/plues/ui/components/ResultBoxFactory.java
@@ -1,8 +1,9 @@
 package de.hhu.stups.plues.ui.components;
 
 import de.hhu.stups.plues.prob.FeasibilityResult;
+
 import javafx.concurrent.Worker;
 
 public interface ResultBoxFactory {
-    ResultBox create(Worker<FeasibilityResult> task);
+  ResultBox create(Worker<FeasibilityResult> task);
 }
