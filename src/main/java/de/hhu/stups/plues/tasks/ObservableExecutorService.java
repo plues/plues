e7e3407fafa012cfa2f0264e40580fe995d64fb9
David Schneider <david.schneider@bivab.de>
2016-08-16 11:33:04 +0200

    Typos and a bit of documentation

--- src/main/java/de/hhu/stups/plues/tasks/ObservableExecutorService.java
+++ src/main/java/de/hhu/stups/plues/tasks/ObservableExecutorService.java
@@ -10,91 +10,95 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * A wrapper class for ExecutorService instances that is observable.
+ * Observers get notified whenever a new work item is submitted.
+ */
 public class ObservableExecutorService extends Observable implements ExecutorService {
-  private final ExecutorService excecutorService;
+  private final ExecutorService executorService;
 
   public ObservableExecutorService(final ExecutorService service) {
-    this.excecutorService = service;
+    this.executorService = service;
   }
 
   @Override
   public void shutdown() {
-    excecutorService.shutdown();
+    executorService.shutdown();
   }
 
   @Override
   public List<Runnable> shutdownNow() {
-    return excecutorService.shutdownNow();
+    return executorService.shutdownNow();
   }
 
   @Override
   public boolean isShutdown() {
-    return excecutorService.isShutdown();
+    return executorService.isShutdown();
   }
 
   @Override
   public boolean isTerminated() {
-    return excecutorService.isTerminated();
+    return executorService.isTerminated();
   }
 
   @Override
   public boolean awaitTermination(final long timeout, final TimeUnit unit) throws InterruptedException {
-    return excecutorService.awaitTermination(timeout, unit);
+    return executorService.awaitTermination(timeout, unit);
   }
 
   @Override
   public <T> Future<T> submit(final Callable<T> task) {
     this.setChanged();
     this.notifyObservers(task);
-    return excecutorService.submit(task);
+    return executorService.submit(task);
   }
 
   @Override
   public <T> Future<T> submit(final Runnable task, final T result) {
     this.setChanged();
     this.notifyObservers(task);
-    return excecutorService.submit(task, result);
+    return executorService.submit(task, result);
   }
 
   @Override
   public Future<?> submit(final Runnable task) {
     this.setChanged();
     this.notifyObservers(task);
-    return excecutorService.submit(task);
+    return executorService.submit(task);
   }
 
   @Override
   public <T> List<Future<T>> invokeAll(final Collection<? extends Callable<T>> tasks) throws InterruptedException {
     this.setChanged();
     this.notifyObservers(tasks);
-    return excecutorService.invokeAll(tasks);
+    return executorService.invokeAll(tasks);
   }
 
   @Override
   public <T> List<Future<T>> invokeAll(final Collection<? extends Callable<T>> tasks, final long timeout, final TimeUnit unit) throws InterruptedException {
     this.setChanged();
     this.notifyObservers(tasks);
-    return excecutorService.invokeAll(tasks, timeout, unit);
+    return executorService.invokeAll(tasks, timeout, unit);
   }
 
   @Override
   public <T> T invokeAny(final Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
     this.setChanged();
     this.notifyObservers(tasks);
-    return excecutorService.invokeAny(tasks);
+    return executorService.invokeAny(tasks);
   }
 
   @Override
   public <T> T invokeAny(final Collection<? extends Callable<T>> tasks, final long timeout, final TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
     this.setChanged();
     this.notifyObservers(tasks);
-    return excecutorService.invokeAny(tasks, timeout, unit);
+    return executorService.invokeAny(tasks, timeout, unit);
   }
 
   @Override
   public void execute(final Runnable command) {
     this.setChanged();
     this.notifyObservers(command);
-    excecutorService.execute(command);
+    executorService.execute(command);
   }
 }
