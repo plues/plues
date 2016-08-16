04e64654dc94a5cbdb2f7823a05dba481bda7711
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-08-16 16:13:16 +0200

    Move pdf generation into new service and bind properties to service instead of solverTask

--- src/main/java/de/hhu/stups/plues/ui/components/ResultBox.java
+++ src/main/java/de/hhu/stups/plues/ui/components/ResultBox.java
@@ -3,11 +3,9 @@ package de.hhu.stups.plues.ui.components;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
-import de.hhu.stups.plues.Delayed;
-import de.hhu.stups.plues.data.Store;
 import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.prob.FeasibilityResult;
-import de.hhu.stups.plues.studienplaene.Renderer;
+import de.hhu.stups.plues.tasks.SolverTask;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
 
@@ -16,9 +14,7 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.binding.StringBinding;
-import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
@@ -33,23 +29,17 @@ import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 
 import org.controlsfx.control.Notifications;
-import org.xml.sax.SAXException;
 
 import java.awt.Desktop;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 
 import javax.swing.SwingUtilities;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class ResultBox extends GridPane implements Initializable {
 
@@ -59,13 +49,10 @@ public class ResultBox extends GridPane implements Initializable {
   private static final String SUCCESS_COLOR = "#DFF2BF";
   private static final String WORKING_COLOR = "#BDE5F8";
 
-  private final Task<FeasibilityResult> task;
-  private final BooleanProperty feasible;
   private final ObjectProperty<Course> majorCourse;
   private final ObjectProperty<Course> minorCourse;
-  private final Delayed<Store> delayedStore;
-  private final ExecutorService executor;
   private final ObjectProperty<Path> pdf;
+  private final PdfRenderingService service;
 
   @FXML
   private StackPane statePane;
@@ -99,49 +86,19 @@ public class ResultBox extends GridPane implements Initializable {
    */
   @Inject
   public ResultBox(final FXMLLoader loader,
-                   final ExecutorService executor,
-                   final Delayed<Store> delayedStore,
+                   final PdfRenderingServiceFactory factory,
                    @Assisted final Task<FeasibilityResult> task) {
     super();
     this.majorCourse = new SimpleObjectProperty<>();
     this.minorCourse = new SimpleObjectProperty<>();
-    this.feasible = new SimpleBooleanProperty(false);
     this.pdf = new SimpleObjectProperty<>();
-    this.task = task;
-    this.executor = executor;
-    this.delayedStore = delayedStore;
-    this.feasible.bind( // set if task has a value
-        Bindings.createBooleanBinding(() -> true, task.valueProperty()));
+    this.service = factory.create((SolverTask<FeasibilityResult>) task);
 
     loader.setLocation(this.getClass()
         .getResource("/fxml/components/resultbox.fxml"));
 
     loader.setRoot(this);
     loader.setController(this);
-
-    task.setOnSucceeded(event -> {
-      if (feasible.get()) {
-        final Course major = majorCourse.get();
-        final Course minor = minorCourse.get();
-        FeasibilityResult result = null;
-        try {
-          result = task.get();
-        } catch (InterruptedException | ExecutionException exc) {
-          exc.printStackTrace();
-        }
-        final FeasibilityResult finalResult = result;
-        Task<Path> pdfCreationTask = new Task<Path>() {
-          @Override
-          protected Path call() throws Exception {
-            return handlePdf(major, minor, finalResult);
-          }
-        };
-        pdfCreationTask.setOnSucceeded(creationEvent ->
-            Platform.runLater(() ->
-              pdf.set((Path) creationEvent.getSource().getValue())));
-        submit(pdfCreationTask);
-      }
-    });
 
     try {
       loader.load();
@@ -158,10 +115,23 @@ public class ResultBox extends GridPane implements Initializable {
     this.minor.textProperty()
         .bind(Bindings.selectString(this.minorCourse, "fullName"));
     //
+    service.majorProperty().bind(this.majorCourse);
+    service.minorProperty().bind(this.minorCourse);
+    service.setOnSucceeded(event -> {
+      Platform.runLater(() ->
+          pdf.set((Path) event.getSource().getValue()));
+    });
+    service.setOnFailed(event -> {
+      Notifications message = Notifications.create();
+      message.title("Error! Could not generate PDF");
+      message.text(event.getSource().getException().getMessage());
+      message.show();
+    });
+    //
     this.progressIndicator.setStyle(
         " -fx-progress-color: " + WORKING_COLOR);
     this.progressIndicator.visibleProperty()
-        .bind(this.task.runningProperty());
+        .bind(service.runningProperty());
     //
     // Binding the progress property of the indicator shows a the percentage
     // of completion which in this case is arbitrary since we do not know how
@@ -172,70 +142,26 @@ public class ResultBox extends GridPane implements Initializable {
     //
     this.show.disableProperty().bind(p);
     this.save.disableProperty().bind(p);
-    this.cancel.disableProperty().bind(task.runningProperty().not());
+    this.cancel.disableProperty().bind(service.runningProperty().not());
     //
     this.icon.graphicProperty().bind(this.getIconBinding());
     this.icon.styleProperty().bind(this.getStyleBinding());
-  }
-
-  /**
-   * Helper method to get temporary file.
-   * @param renderer Renderer object to create file
-   * @param temp Temporary file
-   */
-  private void getTempFile(Renderer renderer, File temp) {
-    try (OutputStream out = new FileOutputStream(temp)) {
-      renderer.getResult().writeTo(out);
-    } catch (final IOException exc) {
-      Notifications message = Notifications.create();
-      message.text("Error! Temporary file was not found.");
-      message.show();
-      exc.printStackTrace();
-    } catch (final ParserConfigurationException exc) {
-      Notifications message = Notifications.create();
-      message.text("Error! Parsing failed.");
-      message.show();
-      exc.printStackTrace();
-    } catch (final SAXException exc) {
-      Notifications message = Notifications.create();
-      message.text("Error! API throws an exception. Contact development.");
-      message.show();
-      exc.printStackTrace();
-    }
-  }
-
-  private File getTempFile(Course major, Course minor) {
-    try {
-      if (minor == null) {
-        return File.createTempFile(getDocumentName(major), ".pdf");
-      } else {
-        return File.createTempFile(getDocumentName(major, minor), ".pdf");
-      }
-    } catch (IOException exc) {
-      Notifications message = Notifications.create();
-      message.text("Error! Creation of temporary file failed");
-      message.show();
-      exc.printStackTrace();
-      return null;
-    }
+    //
+    service.start();
   }
 
   private StringBinding getStyleBinding() {
     return Bindings.createStringBinding(() -> {
       String color = null;
 
-      switch (this.task.getState()) {
+      switch (service.getState()) {
         case READY:
         case SCHEDULED:
         case RUNNING:
           return "";
 
         case SUCCEEDED:
-          if (this.feasible.get()) {
-            color = SUCCESS_COLOR;
-          } else {
-            color = FAILURE_COLOR;
-          }
+          color = SUCCESS_COLOR;
           break;
         case CANCELLED:
           color = WARNING_COLOR;
@@ -249,25 +175,21 @@ public class ResultBox extends GridPane implements Initializable {
 
       return "-fx-background-color: " + color;
 
-    }, this.task.stateProperty(), this.feasible);
+    }, service.stateProperty());
   }
 
   private ObjectBinding<Text> getIconBinding() {
     return Bindings.createObjectBinding(() -> {
       FontAwesomeIcon symbol = null;
 
-      switch (this.task.getState()) {
+      switch (service.getState()) {
         case READY:
         case SCHEDULED:
         case RUNNING:
           return null;
 
         case SUCCEEDED:
-          if (this.feasible.get()) {
-            symbol = FontAwesomeIcon.CHECK;
-          } else {
-            symbol = FontAwesomeIcon.REMOVE;
-          }
+          symbol = FontAwesomeIcon.CHECK;
           break;
         case CANCELLED:
           symbol = FontAwesomeIcon.QUESTION;
@@ -283,26 +205,7 @@ public class ResultBox extends GridPane implements Initializable {
           = FontAwesomeIconFactory.get();
       return iconFactory.createIcon(symbol, ICON_SIZE);
 
-    }, this.task.stateProperty(), this.feasible);
-  }
-
-  /**
-   * Handle pdf file. If show is true, create a temporary file and open it, if false, save this temp
-   * file where ever the user wants to to be saved.
-   * @param major Course object representing major course
-   * @param minor Course object representing minor course. Could be null if major is integrated
-   * @param result Instance of FeasiblityResult containing choice maps
-   */
-  private Path handlePdf(Course major, Course minor, FeasibilityResult result) {
-    final Store store = delayedStore.get();
-    final Renderer renderer
-        = new Renderer(store, result.getGroupChoice(), result.getSemesterChoice(),
-        result.getModuleChoice(), result.getUnitChoice(), major, "true");
-
-    File tmp = getTempFile(major, minor);
-
-    getTempFile(renderer, tmp);
-    return Paths.get(tmp.getAbsolutePath());
+    }, service.stateProperty());
   }
 
   /**
@@ -370,7 +273,7 @@ public class ResultBox extends GridPane implements Initializable {
 
   @FXML
   final void interrupt() {
-    this.task.cancel();
+    this.service.cancel();
   }
 
   public final void setMajorCourse(final Course majorCourse) {
@@ -379,9 +282,5 @@ public class ResultBox extends GridPane implements Initializable {
 
   public final void setMinorCourse(final Course minorCourse) {
     this.minorCourse.set(minorCourse);
-  }
-
-  public final void submit(final Task<?> command) {
-    this.executor.submit(command);
   }
 }
