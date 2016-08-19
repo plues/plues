904314e5ed3e3435a8e8f8c4985e8186a681e508
David Schneider <david.schneider@bivab.de>
2016-08-19 17:07:32 +0200

    Added test for success case in ResultBox

---
+++ src/test/java/de/hhu/stups/plues/ui/components/SuccessfulResultBoxTest.java
@@ -0,0 +1,41 @@
+package de.hhu.stups.plues.ui.components;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
+import javafx.concurrent.Task;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class SuccessfulResultBoxTest extends ResultBoxTest {
+
+  private static final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
+
+  /**
+   * Default constructor.
+   */
+  public SuccessfulResultBoxTest() {
+    super();
+    this.setService(new TestPdfService());
+    this.setIcon(FontAwesomeIconFactory.get().createIcon(FontAwesomeIcon.CHECK, "50"));
+  }
+
+  private static final class TestPdfService extends PdfRenderingService {
+    TestPdfService() {
+      super(null, null, EXECUTOR);
+    }
+
+    @Override
+    public Task<Path> createTask() {
+      return new Task() {
+        @Override
+        protected Object call() throws Exception {
+          return Paths.get(".");
+        }
+      };
+    }
+  }
+}
