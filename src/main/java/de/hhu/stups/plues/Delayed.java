bf4dd82e54829a36309dd918daeaaccece6ba961
David Schneider <david.schneider@bivab.de>
2016-08-25 08:48:45 +0200

    Delayed.get raises a NullPointerException on access if the delayed value has not been set yet.
    Additionally formatting and javadoc.

--- src/main/java/de/hhu/stups/plues/Delayed.java
+++ src/main/java/de/hhu/stups/plues/Delayed.java
@@ -8,39 +8,54 @@ import java.util.function.Consumer;
 
 
 /**
- * Delayed<T> represents an instance of T that becomes available with an
+ * Delayed represents an instance of T that becomes available with an
  * arbitrary delay.
  * Consumers can register Event handlers to be executed once the T object
  * is available.
  * If the T object is already set, new event handlers are executed immediately.
- *
- * @param <T>
  */
 public class Delayed<T> implements Provider<T> {
-    private T instance;
-    private final List<Consumer<T>> listeners;
+  private final List<Consumer<T>> listeners;
+  private T instance;
 
-    public Delayed() {
-        this.listeners = new LinkedList<>();
-    }
+  public Delayed() {
+    this.listeners = new LinkedList<>();
+  }
 
-    public final synchronized void whenAvailable(final Consumer<T> r) {
-        if(this.instance != null) {
-            r.accept(this.instance);
-        } else {
-            this.listeners.add(r);
-        }
+  /**
+   * Register a consumer executed once the deleyaed object has been set, or immediately if it was
+   * aleready set.
+   * @param consumer Consumer
+   */
+  public final synchronized void whenAvailable(final Consumer<T> consumer) {
+    if (this.instance != null) {
+      consumer.accept(this.instance);
+    } else {
+      this.listeners.add(consumer);
     }
+  }
 
-    public final synchronized void set(final T i) {
-        this.instance = i;
-        for(final Consumer<T> r : this.listeners) {
-            r.accept(this.instance);
-        }
-        this.listeners.clear();
+  /**
+   * Set the delayed object to the passed value and trigger registered.
+   * @param instance of type T
+   */
+  public final synchronized void set(final T instance) {
+    this.instance = instance;
+    for (final Consumer<T> r : this.listeners) {
+      r.accept(this.instance);
     }
+    this.listeners.clear();
+  }
 
-    public final synchronized T get() {
-        return this.instance;
+  /**
+   * Read the delayed values, assuming it has been set.
+   * @return T the value stored in the instance.
+   * @throws NullPointerException if the object has not been set.
+   */
+  public final synchronized T get() {
+    if (this.instance == null) {
+      throw new NullPointerException("Delayed object has not been initialized yet");
     }
+    return this.instance;
+  }
 }
