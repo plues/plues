820613c9a8bff15a8e37ca87e87e90a2fd10cc96
David Schneider <david.schneider@bivab.de>
2016-08-26 09:23:31 +0200

    Formatting

--- src/main/java/de/hhu/stups/plues/injector/PropertiesModule.java
+++ src/main/java/de/hhu/stups/plues/injector/PropertiesModule.java
@@ -1,5 +1,7 @@
 package de.hhu.stups.plues.injector;
 
+import static java.lang.Thread.currentThread;
+
 import com.google.inject.AbstractModule;
 
 import java.io.FileNotFoundException;
@@ -7,53 +9,47 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import static java.lang.Thread.currentThread;
-
 class PropertiesModule extends AbstractModule {
-    private static Properties setupProperties() {
-        final Properties defaults = new Properties();
-        final Properties properties = loadProperties(defaults, "main", "local");
 
-        // settings that can be overriden in env vars
-        final String[] env = new String[]{"MODELPATH", "DBPATH"};
+  private static Properties setupProperties() {
+    final Properties defaults = new Properties();
+    final Properties properties = loadProperties(defaults, "main", "local");
 
-        for(final String it : env) {
-            if(System.getenv(it) != null) {
-                properties.setProperty(it.toLowerCase(), System.getenv(it));
-            }
+    // settings that can be overriden in env vars
+    final String[] env = new String[] {"MODELPATH", "DBPATH"};
+
+    for (final String it : env) {
+      if (System.getenv(it) != null) {
+        properties.setProperty(it.toLowerCase(), System.getenv(it));
+      }
+    }
+    return properties;
+  }
+
+  private static Properties loadProperties(final Properties properties,
+                                           final String... propertyFiles) {
+    for (final String propertyFile : propertyFiles) {
+      try {
+        final ClassLoader classLoader = currentThread().getContextClassLoader();
+        final InputStream p = classLoader.getResourceAsStream(propertyFile + ".properties");
+
+        if (p == null) {
+          continue;
         }
-        return properties;
+
+        properties.load(p);
+      } catch (final FileNotFoundException exception) {
+        System.err.println(propertyFile + ".properties is missing!");
+      } catch (final IOException exception) {
+        System.err.println(propertyFile + ".properties produced IO Error!");
+      }
     }
-
-    private static Properties loadProperties(final Properties properties,
-                                             final String... propertyFiles) {
-        for(final String propertyFile : propertyFiles) {
-            try {
-                final ClassLoader classLoader = currentThread()
-                                                        .getContextClassLoader();
-                final InputStream p = classLoader
-                                              .getResourceAsStream(propertyFile
-                                                                           + ".properties");
-
-                if(p == null) {
-                    continue;
-                }
-
-                properties.load(p);
-            } catch (final FileNotFoundException e) {
-                System.err.println(propertyFile
-                                           + ".properties is missing!");
-            } catch (final IOException e) {
-                System.err.println(propertyFile
-                                           + ".properties produced IO Error!");
-            }
-        }
-        return properties;
-    }
+    return properties;
+  }
 
 
-    @Override
-    public void configure() {
-        bind(Properties.class).toInstance(setupProperties());
-    }
+  @Override
+  public void configure() {
+    bind(Properties.class).toInstance(setupProperties());
+  }
 }
