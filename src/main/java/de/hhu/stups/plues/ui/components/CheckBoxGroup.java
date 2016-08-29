9208692b8b54c45d49bccdbca9dbb65c4f904b31
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-08-29 14:24:29 +0200

    Fix problem with properties

--- src/main/java/de/hhu/stups/plues/ui/components/CheckBoxGroup.java
+++ src/main/java/de/hhu/stups/plues/ui/components/CheckBoxGroup.java
@@ -13,14 +13,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.stream.Stream;
 
-import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -71,27 +72,25 @@ public class CheckBoxGroup extends VBox implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     courseField.setText(course.getFullName());
-    CheckBox[] list = new CheckBox[boxToUnit.size()];
+    ObservableList<Node> children = unitsBox.getChildren();
+    BooleanProperty[] properties = new BooleanProperty[boxToUnit.size()];
+
     int i = 0;
     for (Map.Entry<CheckBox, AbstractUnit> entry : boxToUnit.entrySet()) {
       CheckBox cb = entry.getKey();
       cb.setText(entry.getValue().getTitle());
-      cb.setSelected(moduleBox.isSelected());
-      unitsBox.getChildren().add(cb);
-      list[i] = cb;
-      i++;
+      children.add(cb);
+      properties[i++] = cb.selectedProperty();
     }
 
     moduleBox.setText(module.getTitle());
     BooleanBinding allSelected = Bindings.createBooleanBinding(() ->
-      Stream.of(list).allMatch(CheckBox::isSelected),
-      Stream.of(list).map(CheckBox::selectedProperty).toArray(Observable[]::new));
+      children.stream().allMatch(node -> ((CheckBox) node).isSelected()), properties);
 
-    allSelected.addListener((obs, wereAllSelected, areAllNowSelected) ->
-      moduleBox.setSelected(areAllNowSelected));
+    allSelected.addListener((observable, oldValue, newValue) -> moduleBox.setSelected(newValue));
 
     moduleBox.setOnAction(e ->
-      Stream.of(list).forEach(box -> box.setSelected(moduleBox.isSelected())));
+      children.forEach(box -> ((CheckBox) box).setSelected(moduleBox.isSelected())));
   }
   public HashMap<CheckBox, AbstractUnit> getBoxToUnit() {
     return boxToUnit;
