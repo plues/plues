9aa9d3a24123a3b94cc340457c08179fb9b6be65
Tobias Witt <tobias.witt@hhu.de>
2016-09-12 12:01:26 +0200

    Render sessions into session slots

--- src/main/java/de/hhu/stups/plues/ui/components/Timetable.java
+++ src/main/java/de/hhu/stups/plues/ui/components/Timetable.java
@@ -1,19 +1,31 @@
 package de.hhu.stups.plues.ui.components;
 
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.THURSDAY;
+import static java.time.DayOfWeek.TUESDAY;
+import static java.time.DayOfWeek.WEDNESDAY;
+
 import com.google.inject.Inject;
 
 import de.hhu.stups.plues.Delayed;
 import de.hhu.stups.plues.data.Store;
 import de.hhu.stups.plues.data.entities.Course;
+import de.hhu.stups.plues.data.entities.Session;
 import de.hhu.stups.plues.tasks.SolverService;
 import de.hhu.stups.plues.tasks.SolverTask;
 import de.hhu.stups.plues.ui.layout.Inflater;
 
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.ListBinding;
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -24,6 +36,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.time.DayOfWeek;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
@@ -53,6 +67,7 @@ public class Timetable extends BorderPane implements Initializable {
 
   private SolverService solverService;
 
+  private ListProperty<Session> sessions = new SimpleListProperty<>();
 
   /**
    * Timetable component.
@@ -73,6 +88,8 @@ public class Timetable extends BorderPane implements Initializable {
     this.delayedStore.whenAvailable(s -> {
       Runtime.getRuntime().addShutdownHook(new Thread(s::close));
       this.courseFilter.setCourses(s.getCourses());
+
+      setSessions(s.getSessions());
     });
 
     this.courseProperty.bind(this.courseFilter.selectedItemProperty());
@@ -92,12 +109,35 @@ public class Timetable extends BorderPane implements Initializable {
   }
 
   private void initSessionBoxes() {
-    final int offX = 1, offY = 1, widthX = 5;
+    final int offX = 1;
+    final int offY = 1;
+    final int widthX = 5;
 
     IntStream.range(0, 35).forEach(i -> {
-      ListView view = new ListView();
+
+      ListView<Session> view = new ListView<>();
+      view.itemsProperty().bind(new ListBinding<Session>() {
+        {
+          // TODO: bind to all slot properties and to new slot properties
+          // TODO: http://stackoverflow.com/questions/32536096/javafx-bindings-not-working-as-expected
+          bind(sessions);
+        }
+
+        @Override
+        protected ObservableList<Session> computeValue() {
+          return sessions.filtered(session -> session.getSlot().equals(getSlot(i, widthX)));
+        }
+      });
+
       timeTable.add(view, i % widthX + offX, (i / widthX) + offY);
     });
+  }
+
+  private Session.Slot getSlot(int index, int widthX) {
+    DayOfWeek[] days = { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY };
+    Integer[] times = { 1, 2, 3, 4, 5, 6, 7 };
+
+    return new Session.Slot(days[index % widthX], times[index / widthX]);
   }
 
   @FXML
@@ -115,5 +155,10 @@ public class Timetable extends BorderPane implements Initializable {
       logger.info(course.getName() + ": " + i.toString());
     });
     s.submit(t);
+  }
+
+  private void setSessions(List<Session> sessions) {
+    sessions.forEach(Session::initSlotProperty);
+    this.sessions.set(FXCollections.observableArrayList(sessions));
   }
 }
