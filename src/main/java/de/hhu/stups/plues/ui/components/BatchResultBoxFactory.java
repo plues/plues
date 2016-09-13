f42fe95fbea9aa2b40362b267c675f43fc76c986
schmidt <joshua.schmidt@uni-duesseldorf.de>
2016-09-13 12:03:07 +0200

    remove assisted anotation

--- src/main/java/de/hhu/stups/plues/ui/components/BatchResultBoxFactory.java
+++ src/main/java/de/hhu/stups/plues/ui/components/BatchResultBoxFactory.java
@@ -4,13 +4,11 @@ import com.google.inject.assistedinject.Assisted;
 
 import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.tasks.PdfRenderingTask;
-import javafx.scene.layout.VBox;
 
 import java.nio.file.Path;
 import java.util.Set;
 
 public interface BatchResultBoxFactory {
   BatchResultBox create(@Assisted("major") Course major, @Assisted("minor") Course minor,
-                        @Assisted Path tempDirectoryPath,
-                        @Assisted Set<PdfRenderingTask> taskPool);
+                        Path tempDirectoryPath, Set<PdfRenderingTask> taskPool);
 }
