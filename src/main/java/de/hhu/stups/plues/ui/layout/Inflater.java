9396303cf24208dbf3ac97a5ce3502a9b91bb3dd
David Schneider <david.schneider@bivab.de>
2016-09-20 20:13:51 +0200

    Marked fields as final and log exception if opening an error occurs when opening an fxml file

--- src/main/java/de/hhu/stups/plues/ui/layout/Inflater.java
+++ src/main/java/de/hhu/stups/plues/ui/layout/Inflater.java
@@ -8,6 +8,8 @@ import javafx.scene.Parent;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Inflates a layout from a filename.
@@ -17,25 +19,27 @@ public class Inflater {
   private final FXMLLoader loader;
 
   @Inject
-  public Inflater(FXMLLoader loader) {
+  public Inflater(final FXMLLoader loader) {
     this.loader = loader;
   }
 
   /**
    * Inflate a fxml resource as a layout from <tt>/fxml/</tt>.
+   *
    * @param name The name of the xml file without the <tt>.xml</tt> extension.
    * @return {@link Parent}
    */
-  public Parent inflate(String name) {
+  public Parent inflate(final String name) {
     return inflate(name, null, null);
   }
 
   /**
    * Inflate a fxml resource as a layout from <tt>/fxml/</tt>.
+   *
    * @param name The name of the xml file without the <tt>.xml</tt> extension.
    * @param root optional root node to inflate this layout into
    */
-  public Parent inflate(String name, Parent root, Object controller) {
+  public Parent inflate(final String name, final Parent root, final Object controller) {
     // set location explicitly to ensure using the injected fxml loader
     loader.setLocation(getClass().getResource("/fxml/" + name + ".fxml"));
 
@@ -47,16 +51,16 @@ public class Inflater {
       loader.setController(controller);
     }
 
-    ResourceBundle bundle = ResourceBundle.getBundle("lang.plues", new Locale("de"));
+    final ResourceBundle bundle = ResourceBundle.getBundle("lang.plues", new Locale("de"));
     loader.setResources(bundle);
 
     try {
       return loader.load();
-    } catch (IOException ignored) {
-      ignored.printStackTrace();
+    } catch (final IOException ignored) {
+      final Logger logger = Logger.getLogger(getClass().getSimpleName());
+      logger.log(Level.SEVERE, "Exception in FXML Loader", ignored);
+      // TODO: kill app!
+      throw new RuntimeException(ignored);
     }
-
-    // TODO: kill app!
-    return null;
   }
 }
