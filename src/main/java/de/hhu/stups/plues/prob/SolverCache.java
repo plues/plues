864b645330fe94b90d0d101bb89c09b52e143e72
David Schneider <david.schneider@bivab.de>
2016-09-20 18:52:53 +0200

    Added custom equals and hashCode methods to SolverCache

--- src/main/java/de/hhu/stups/plues/prob/SolverCache.java
+++ src/main/java/de/hhu/stups/plues/prob/SolverCache.java
@@ -2,17 +2,18 @@ package de.hhu.stups.plues.prob;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 class SolverCache extends LinkedHashMap<String,Object> {
 
-  private int cacheSize;
+  private final int cacheSize;
 
   /**
    * Cache the results computed by the solver to increase performance. Since the solver gets
    * injected and is single threaded by default we probably would have no problems without
    * synchronization but never the less accessing the cache should be done in a synchronized block.
    */
-  SolverCache(int cacheSize) {
+  SolverCache(final int cacheSize) {
     super(cacheSize, 0.75f, true);
     this.cacheSize = cacheSize;
   }
@@ -24,7 +25,27 @@ class SolverCache extends LinkedHashMap<String,Object> {
    * @return Return true if the cache size is exhausted otherwise false.
    */
   @Override
-  protected boolean removeEldestEntry(Map.Entry<String, Object> eldestEntry) {
+  protected boolean removeEldestEntry(final Map.Entry<String, Object> eldestEntry) {
     return this.size() > cacheSize;
+  }
+
+  @Override
+  public boolean equals(final Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+    if (!super.equals(other)) {
+      return false;
+    }
+    final SolverCache that = (SolverCache) other;
+    return cacheSize == that.cacheSize;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), cacheSize);
   }
 }
