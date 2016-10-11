71d836ee0ad37be5c27d407629bc72b95f55e201
schmidt <joshua.schmidt@uni-duesseldorf.de>
2016-10-11 16:26:22 +0200

    show abstract units with units that have no semesters in common, redundant unit groups and quasi-mandatory modules for each course in the reports

--- src/main/java/de/hhu/stups/plues/ui/controller/Reports.java
+++ src/main/java/de/hhu/stups/plues/ui/controller/Reports.java
@@ -6,15 +6,15 @@ import de.hhu.stups.plues.Delayed;
 import de.hhu.stups.plues.data.Store;
 import de.hhu.stups.plues.data.entities.AbstractUnit;
 import de.hhu.stups.plues.data.entities.Course;
+import de.hhu.stups.plues.data.entities.Group;
 import de.hhu.stups.plues.data.entities.Module;
 import de.hhu.stups.plues.data.entities.Unit;
+import de.hhu.stups.plues.prob.ReportData;
 import de.hhu.stups.plues.prob.report.Pair;
 import de.hhu.stups.plues.tasks.SolverService;
 import de.hhu.stups.plues.tasks.SolverTask;
 import de.hhu.stups.plues.ui.layout.Inflater;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
@@ -29,8 +29,9 @@ import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -38,13 +39,13 @@ import java.util.stream.Collectors;
 
 class Reports extends VBox implements Initializable {
 
-  private final BooleanProperty solverProperty = new SimpleBooleanProperty(false);
   private final List<AbstractUnit> abstractUnitsWithoutUnits;
   private final List<Unit> units;
   private final List<AbstractUnit> abstractUnits;
   private final List<Course> courses;
-  private final Set<String> impossibleCourses;
   private final List<Module> mandatoryModules;
+  private final Map<Integer, Set<Integer>> quasiMandatoryModules;
+  private final List<Group> groups;
   private int groupAmount;
   private int sessionAmount;
 
@@ -69,6 +70,9 @@ class Reports extends VBox implements Initializable {
   @FXML
   @SuppressWarnings("unused")
   private TitledPane paneQuasiMandatoryModules;
+  @FXML
+  @SuppressWarnings("unused")
+  private TitledPane paneRedundantUnitGroups;
   @FXML
   @SuppressWarnings("unused")
   private Label lbCourseAmount;
@@ -98,6 +102,9 @@ class Reports extends VBox implements Initializable {
   private TableView<Pair<String>> tableViewAbstractUnitsWithUnits;
   @FXML
   @SuppressWarnings("unused")
+  private TableView<Pair<String>> tableViewRedundantUnitGroups;
+  @FXML
+  @SuppressWarnings("unused")
   private TableColumn<Pair<String>, String> tableColumnCourseName;
   @FXML
   @SuppressWarnings("unused")
@@ -108,6 +115,18 @@ class Reports extends VBox implements Initializable {
   @FXML
   @SuppressWarnings("unused")
   private TableColumn<Pair<String>, String> tableColumnAbstractTitle;
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Pair<String>, String> tableColumnAbstractUnit;
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Pair<String>, String> tableColumnUnit;
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Pair<String>, String> tableColumnRedundantUnit;
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Pair<String>, String> tableColumnRedundantGroupPair;
   @FXML
   @SuppressWarnings("unused")
   private ListView<String> listViewCourses;
@@ -129,62 +148,37 @@ class Reports extends VBox implements Initializable {
   public Reports(final Inflater inflater, final Delayed<Store> delayedStore,
                  final Delayed<SolverService> delayedSolverService,
                  final ExecutorService executor) {
-    impossibleCourses = new HashSet<>();
     courses = new ArrayList<>();
     units = new ArrayList<>();
     abstractUnits = new ArrayList<>();
     abstractUnitsWithoutUnits = new ArrayList<>();
     mandatoryModules = new ArrayList<>();
+    groups = new ArrayList<>();
+    quasiMandatoryModules = new HashMap<>();
 
     delayedStore.whenAvailable(store -> {
+      groups.addAll(store.getGroups());
       courses.addAll(store.getCourses());
       units.addAll(store.getUnits());
       mandatoryModules.addAll(store.getModules());
       abstractUnits.addAll(store.getAbstractUnits());
-      groupAmount = store.getGroups().size();
+      groupAmount = groups.size();
       sessionAmount = store.getSessions().size();
       abstractUnitsWithoutUnits.addAll(abstractUnits.stream()
           .filter(abstractUnit -> abstractUnit.getUnits().isEmpty()).collect(Collectors.toList()));
     });
 
     delayedSolverService.whenAvailable(solverService -> {
-      final SolverTask<Set<String>> impossibleCoursesTask = solverService.impossibleCoursesTask();
-      impossibleCoursesTask.setOnSucceeded(event -> {
-        impossibleCourses.addAll(impossibleCoursesTask.getValue());
-        tableViewImpossibleCourses.getItems().addAll(
-            impossibleCourses.stream()
-                .map(course -> new Pair<>(course, getFullName(course)))
-                .collect(Collectors.toList()));
-        solverProperty.set(true);
-        lbImpossibleCoursesAmount.setText(String.valueOf(impossibleCourses.size()));
-      });
-      executor.submit(impossibleCoursesTask);
+      final SolverTask<ReportData> reportDataTask = solverService.collectReportDataTask();
+      reportDataTask.setOnSucceeded(event -> displayReportData(reportDataTask.getValue()));
+      executor.submit(reportDataTask);
     });
 
     inflater.inflate("Reports", this, this, "reports");
   }
 
-  /**
-   * Get the courses full name for a given course name.
-   *
-   * @param courseName The course's name.
-   * @return Return the course's full name.
-   */
-  private String getFullName(String courseName) {
-    return courses.stream()
-        .filter(course -> course.getName().equals(courseName))
-        .collect(Collectors.toList()).get(0).getFullName();
-  }
-
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    paneHeader.visibleProperty().bind(solverProperty);
-    paneImpossibleCourses.visibleProperty().bind(solverProperty);
-    paneAbstractUnits.visibleProperty().bind(solverProperty);
-    paneAbstractUnitsWithUnits.visibleProperty().bind(solverProperty);
-    paneMandatoryModules.visibleProperty().bind(solverProperty);
-    paneQuasiMandatoryModules.visibleProperty().bind(solverProperty);
-
     final String listStyle = "batchListView";
     tableViewImpossibleCourses.setId(listStyle);
     tableViewAbstractUnits.setId(listStyle);
@@ -194,33 +188,33 @@ class Reports extends VBox implements Initializable {
     listViewQuasiCourses.setId(listStyle);
     listViewQuasiMandatoryModules.setId(listStyle);
 
-    tableColumnCourseName.setCellValueFactory(new PropertyValueFactory<>("first"));
-    tableColumnCourseFullName.setCellValueFactory(new PropertyValueFactory<>("second"));
+    final String first = "first";
+    final String second = "second";
+    tableColumnCourseName.setCellValueFactory(new PropertyValueFactory<>(first));
+    tableColumnCourseFullName.setCellValueFactory(new PropertyValueFactory<>(second));
 
-    tableColumnAbstractKey.setCellValueFactory(new PropertyValueFactory<>("first"));
-    tableColumnAbstractTitle.setCellValueFactory(new PropertyValueFactory<>("second"));
+    tableColumnAbstractKey.setCellValueFactory(new PropertyValueFactory<>(first));
+    tableColumnAbstractTitle.setCellValueFactory(new PropertyValueFactory<>(second));
 
+    tableColumnAbstractUnit.setCellValueFactory(new PropertyValueFactory<>(first));
+    tableColumnUnit.setCellValueFactory(new PropertyValueFactory<>(second));
+
+    tableColumnRedundantUnit.setCellValueFactory(new PropertyValueFactory<>(first));
+    tableColumnRedundantGroupPair.setCellValueFactory(new PropertyValueFactory<>(second));
+
+    // add listener to update the (quasi-) mandatory list views according to the selected course
     listViewCourses.getSelectionModel().selectedItemProperty()
         .addListener((observable, oldValue, newValue) -> {
           listViewMandatoryModules.getItems().clear();
           showMandatoryModulesOfCourse(listViewCourses.getSelectionModel().getSelectedItem());
         });
 
-    // Todo: add listener to show quasi-mandatory modules
-
-    // Todo: initialize table view to display abstract units with units that have no semester in common
-
-    tableViewAbstractUnits.getItems()
-        .addAll(abstractUnitsWithoutUnits.stream()
-            .map(unit -> new Pair<>(unit.getKey(), unit.getTitle()))
-            .collect(Collectors.toList()));
-    listViewCourses.getItems()
-        .addAll(courses.stream().map(Course::getName).collect(Collectors.toList()));
-    listViewQuasiCourses.getItems()
-        .addAll(courses.stream().map(Course::getName).collect(Collectors.toList()));
-
-    listViewCourses.getSelectionModel().select(0);
-    listViewQuasiCourses.getSelectionModel().select(0);
+    listViewQuasiCourses.getSelectionModel().selectedItemProperty()
+        .addListener((observable, oldValue, newValue) -> {
+          listViewQuasiMandatoryModules.getItems().clear();
+          showQuasiMandatoryModulesOfCourse(
+              listViewQuasiCourses.getSelectionModel().getSelectedItem());
+        });
 
     lbCourseAmount.setText(String.valueOf(courses.size()));
     lbUnitAmount.setText(String.valueOf(units.size()));
@@ -232,17 +226,117 @@ class Reports extends VBox implements Initializable {
   }
 
   /**
+   * Initialize the list and table views that receive their data from {@link ReportData}.
+   *
+   * @param reportData The {@link ReportData report data} object.
+   */
+  @SuppressWarnings("unused")
+  private void displayReportData(ReportData reportData) {
+    tableViewImpossibleCourses.getItems().addAll(reportData.getImpossibleCourses()
+        .stream().map(courseName ->
+            new Pair<>(courseName, getFullName(courseName))).collect(Collectors.toList()));
+    lbImpossibleCoursesAmount.setText(String.valueOf(reportData.getImpossibleCourses().size()));
+
+    tableViewAbstractUnits.getItems()
+        .addAll(abstractUnitsWithoutUnits.stream()
+            .map(unit -> new Pair<>(unit.getKey(), unit.getTitle()))
+            .collect(Collectors.toList()));
+
+    // get abstract units with units that have no semesters in common
+    for (AbstractUnit abstractUnit : abstractUnits) {
+      abstractUnit.getUnits().stream()
+          .filter(unit -> abstractUnit
+              .getModuleAbstractUnitSemesters().stream()
+              .filter(moduleAbstractUnitSemester ->
+                  unit.getSemesters().contains(moduleAbstractUnitSemester.getSemester()))
+              .collect(Collectors.toList()).isEmpty())
+          .forEach(unit ->
+              tableViewAbstractUnitsWithUnits.getItems()
+                  .add(new Pair<>(abstractUnit.getTitle(), unit.getTitle())));
+    }
+
+    quasiMandatoryModules.putAll(reportData.getQuasiMandatoryModuleAbstractUnits());
+    listViewCourses.getItems()
+        .addAll(courses.stream().map(Course::getName).collect(Collectors.toList()));
+    listViewQuasiCourses.getItems()
+        .addAll(courses.stream().map(Course::getName).collect(Collectors.toList()));
+    listViewCourses.getSelectionModel().select(0);
+    listViewQuasiCourses.getSelectionModel().select(0);
+
+    Map<Integer, Set<Pair<Integer>>> redundantUnitGroups = reportData.getRedundantUnitGroups();
+
+    Set<Unit> redundantUnits = units.stream()
+        .filter(unit -> redundantUnitGroups.containsKey(unit.getId()))
+        .collect(Collectors.toSet());
+
+    redundantUnits.forEach(redundantUnit ->
+        redundantUnitGroups.get(redundantUnit.getId())
+            .forEach(groupPair ->
+                tableViewRedundantUnitGroups.getItems().add(
+                    new Pair<>(redundantUnit.getTitle(), getStringFromPair(groupPair)))));
+  }
+
+  /**
+   * Combine the unit titles of both groups in the given pair to one string.
+   *
+   * @param groupPairIds The pair of group ids.
+   * @return Return a string of the two group unit names.
+   */
+  private String getStringFromPair(Pair<Integer> groupPairIds) {
+    List<Group> groupPair = groups.stream()
+        .filter(group ->
+            group.getId() == groupPairIds.getFirst() || group.getId() == groupPairIds.getSecond())
+        .collect(Collectors.toList());
+    return groupPair.get(0).getUnit().getTitle() + " - " + groupPair.get(1).getUnit().getTitle();
+  }
+
+  /**
+   * Get the full name for a given course name.
+   *
+   * @param courseName The course's name.
+   * @return Return the course's full name.
+   */
+  private String getFullName(String courseName) {
+    return courses.stream()
+        .filter(course -> course.getName().equals(courseName))
+        .collect(Collectors.toList()).get(0).getFullName();
+  }
+
+  /**
+   * Update {@link Reports#listViewQuasiMandatoryModules} to show the quasi-mandatory modules of the
+   * currently selected course in {@link Reports#listViewQuasiCourses}.
+   *
+   * @param selectedCourseName The currently selected course name.
+   */
+  @SuppressWarnings("unused")
+  private void showQuasiMandatoryModulesOfCourse(String selectedCourseName) {
+    Course selectedCourse = courses.stream()
+        .filter(course -> course.getName().equals(selectedCourseName))
+        .collect(Collectors.toList()).get(0);
+    Set<Integer> quasiMandatoryModuleIds = quasiMandatoryModules.get(selectedCourse.getId());
+    if (quasiMandatoryModuleIds != null && !quasiMandatoryModuleIds.isEmpty()) {
+      abstractUnits.forEach(abstractUnit -> {
+        if (quasiMandatoryModuleIds.contains(abstractUnit.getId())) {
+          listViewQuasiMandatoryModules.getItems().addAll(abstractUnit.getTitle());
+        }
+      });
+    } else {
+      listViewQuasiMandatoryModules.getItems().clear();
+    }
+  }
+
+  /**
    * Update {@link Reports#listViewMandatoryModules} to show the mandatory modules of the currently
    * selected course in {@link Reports#listViewCourses}.
    *
-   * @param selectedCourse The currently selected course within {@link Reports#listViewCourses}.
+   * @param selectedCourseName The currently selected course within {@link Reports#listViewCourses}.
    */
   @SuppressWarnings("unused")
-  private void showMandatoryModulesOfCourse(String selectedCourse) {
+  private void showMandatoryModulesOfCourse(String selectedCourseName) {
     mandatoryModules.forEach(module -> {
       if (module.getCourses().stream()
           .map(Course::getName).collect(Collectors.toList())
-          .contains(selectedCourse)) {
+          .contains(selectedCourseName)) {
         listViewMandatoryModules.getItems().add(module.getTitle());
       }
     });
