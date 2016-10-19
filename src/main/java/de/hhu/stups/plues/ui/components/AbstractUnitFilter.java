15e8aa4bf26c093c61f0c997197ad4a5afc39d02
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-10-19 16:07:46 +0200

    First version using search; work in progress. Also, filtering complete, filter for selected units, not selected units and all units

--- src/main/java/de/hhu/stups/plues/ui/components/AbstractUnitFilter.java
+++ src/main/java/de/hhu/stups/plues/ui/components/AbstractUnitFilter.java
@@ -11,96 +11,177 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
-import javafx.event.EventHandler;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.util.Callback;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 
-public class AbstractUnitFilter extends GridPane implements Initializable {
+public class AbstractUnitFilter extends VBox implements Initializable {
 
   private final Delayed<Store> delayedStore;
-  private final ToggleGroup group = new ToggleGroup();
-  private Store store;
+  private final ToggleGroup filterGroup = new ToggleGroup();
+  private ObservableList<RowEntry> allItems = FXCollections.observableArrayList();
+  private ObservableList<RowEntry> displayedItems = FXCollections.observableArrayList();
+  private ObservableList<RowEntry> selectedItems = FXCollections.observableArrayList();
 
   @FXML
   @SuppressWarnings("unused")
-  private RadioButton name;
+  private TextField query;
 
   @FXML
   @SuppressWarnings("unused")
-  private RadioButton semester;
+  private RadioButton selected;
 
   @FXML
   @SuppressWarnings("unused")
-  private ListView<AbstractUnit> units;
+  private RadioButton notSelected;
 
   @FXML
   @SuppressWarnings("unused")
-  private ListView<AbstractUnit> selectedItems;
+  private RadioButton all;
+
+  @FXML
+  @SuppressWarnings("unused")
+  private TableView<RowEntry> units;
 
   @Inject
   public AbstractUnitFilter(final Inflater inflater,
-                            final Delayed<Store> store) {
-    this.delayedStore = store;
+                            final Delayed<Store> delayedStore) {
+    this.delayedStore = delayedStore;
 
     inflater.inflate("components/AbstractUnitFilter", this, this, "main");
   }
 
-  @FXML
-  public void sortByName() {
-    System.out.println("Name");
-    units.getItems().sort((o1, o2) -> o1.getTitle().compareTo(o2.getTitle()));
+  private void sortUnitsByName() {
+    units.getItems().sort((o1, o2) -> ((AbstractUnit) o1.getUnit()).getTitle()
+      .compareTo(((AbstractUnit) o2.getUnit()).getTitle()));
+    units.refresh();
   }
 
   @FXML
-  public void sortBySemester() {
-    System.out.println("Semester");
+  public void search() {
+    ObservableList<RowEntry> filtered = FXCollections.observableArrayList(displayedItems);
+    query.textProperty().addListener((observable, oldValue, newValue) ->
+      filtered.filtered(rowEntry -> {
+        if (newValue == null || newValue.isEmpty()) {
+          return true;
+        }
+
+        if (((AbstractUnit) rowEntry.getUnit()).getTitle().toLowerCase()
+          .contains(newValue.toLowerCase())) {
+          return true;
+        }
+
+        return false;
+      }));
+
+    displayedItems.clear();
+    displayedItems.addAll(filtered);
+    sortUnitsByName();
+
+    all.setSelected(false);
+    selected.setSelected(false);
+    notSelected.setSelected(false);
+  }
+
+  @FXML
+  public void allItems() {
+    displayedItems.clear();
+    displayedItems.addAll(allItems);
+    sortUnitsByName();
+  }
+
+  @FXML
+  public void filterBySelected() {
+    displayedItems.clear();
+    displayedItems.addAll(selectedItems);
+    sortUnitsByName();
+  }
+
+  @FXML
+  public void filterByUnselected() {
+    displayedItems.clear();
+    displayedItems.addAll(allItems.filtered(rowEntry -> !selectedItems.contains(rowEntry)));
+    sortUnitsByName();
   }
 
   @FXML
   public void resetSelection() {
-    selectedItems.getItems().clear();
+    allItems.forEach(rowEntry -> ((CheckBox) rowEntry.getCheckbox()).setSelected(false));
+    selectedItems.clear();
+    selected.setSelected(false);
+    notSelected.setSelected(false);
+    all.setSelected(true);
+    allItems();
   }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    name.setToggleGroup(group);
-    semester.setToggleGroup(group);
+    selected.setToggleGroup(filterGroup);
+    notSelected.setToggleGroup(filterGroup);
+    all.setToggleGroup(filterGroup);
 
-    delayedStore.whenAvailable(store -> {
-      this.store = store;
+    units.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-      Callback<ListView<AbstractUnit>, ListCell<AbstractUnit>> callback = new Callback<ListView<AbstractUnit>, ListCell<AbstractUnit>>() {
-        @Override
-        public ListCell<AbstractUnit> call(ListView<AbstractUnit> param) {
-          return new ListCell<AbstractUnit>() {
-            @Override
-            protected void updateItem(AbstractUnit unit, boolean bool) {
-              super.updateItem(unit, bool);
-              if (unit != null) {
-                setText(unit.getTitle());
-              }
-            }
-          };
-        }
-      };
+    TableColumn<RowEntry, CheckBox> checkBoxTableColumn = new TableColumn<>("Check");
+    TableColumn<RowEntry, String> nameTableColumn = new TableColumn<>("Abstract Unit Title");
 
-      units.getItems().addAll(store.getAbstractUnits());
-      units.setCellFactory(callback);
-      units.setOnMouseClicked(event -> {
-        if (!selectedItems.getItems().contains(units.getSelectionModel().getSelectedItem())) {
-          selectedItems.getItems().add(units.getSelectionModel().property.getSelectedItem());
+    checkBoxTableColumn.setCellValueFactory(new PropertyValueFactory<>("checkbox"));
+    checkBoxTableColumn.setSortable(false);
+    checkBoxTableColumn.setResizable(false);
+    checkBoxTableColumn.setPrefWidth(50);
+    nameTableColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+    nameTableColumn.setSortable(false);
+    nameTableColumn.setPrefWidth(1000);
+    nameTableColumn.setResizable(false);
+
+    units.getColumns().addAll(checkBoxTableColumn, nameTableColumn);
+
+    delayedStore.whenAvailable(store ->
+        store.getAbstractUnits().forEach(abstractUnit -> {
+          allItems.add(new RowEntry<>(new CheckBox(), abstractUnit));
+          allItems();
+          sortUnitsByName();
+        }));
+    units.setItems(displayedItems);
+  }
+
+  public final class RowEntry<T1, T2> {
+    private final T1 checkbox;
+    private final T2 unit;
+    private final String title;
+
+    RowEntry(final T1 checkbox, final T2 unit) {
+      this.checkbox = checkbox;
+      this.unit = unit;
+      this.title = ((AbstractUnit) unit).getTitle();
+      ((CheckBox) checkbox).selectedProperty().addListener((observable, oldValue, newValue) -> {
+        if (newValue) {
+          selectedItems.add(this);
+        } else {
+          selectedItems.remove(this);
         }
       });
+    }
 
-      selectedItems.setCellFactory(callback);
-    });
+    public T1 getCheckbox() {
+      return checkbox;
+    }
 
+    public T2 getUnit() {
+      return unit;
+    }
+
+    public String getTitle() {
+      return title;
+    }
   }
 }
