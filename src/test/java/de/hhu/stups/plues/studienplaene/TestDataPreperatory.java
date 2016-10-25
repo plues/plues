cdacfa95aece4f94534bc57b9462900719be090f
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-10-25 14:49:37 +0200

    Simplify test setup by moving setup into new super class

--- src/test/java/de/hhu/stups/plues/studienplaene/TestDataPreperatory.java
+++ src/test/java/de/hhu/stups/plues/studienplaene/TestDataPreperatory.java
@@ -8,53 +8,22 @@ import de.hhu.stups.plues.data.entities.AbstractUnit;
 import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.data.entities.Group;
 import de.hhu.stups.plues.data.entities.Module;
-import de.hhu.stups.plues.prob.FeasibilityResult;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.net.URISyntaxException;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class TestDataPreperatory {
-
-  private MockStore store;
-  private Course course;
-  private FeasibilityResult result;
+public class TestDataPreperatory extends TestBase {
 
   /**
    * Test setup.
    */
   @Before
-  public void setUp() {
-    store = new MockStore();
-    course = store.getCourseByKey("foo");
-
-    final Map<Integer, Integer> groupChoice = new HashMap<>();
-    groupChoice.put(1, 1);
-    groupChoice.put(2, 12);
-    groupChoice.put(3, 3);
-    groupChoice.put(4, 4);
-    groupChoice.put(5, 11);
-    groupChoice.put(6, 6);
-    groupChoice.put(7, 7);
-    groupChoice.put(8, 10);
-    groupChoice.put(9, 9);
-
-    final Map<Integer, Integer> semesterChoice = new HashMap<>();
-    IntStream.rangeClosed(1, 9).forEach(i -> semesterChoice.put(i, 1));
-
-    final Map<String, Set<Integer>> moduleChoice = new HashMap<>();
-    Set<Integer> integerSet = new HashSet<Integer>();
-    integerSet.add(1);
-    integerSet.add(3);
-    moduleChoice.put("foo", integerSet);
-
-    this.result = new FeasibilityResult(moduleChoice, semesterChoice, groupChoice);
+  public void setUp() throws URISyntaxException {
+    super.setUp();
   }
 
 
--- src/test/java/de/hhu/stups/plues/studienplaene/TestRenderer.java
+++ src/test/java/de/hhu/stups/plues/studienplaene/TestRenderer.java
@@ -2,9 +2,6 @@ package de.hhu.stups.plues.studienplaene;
 
 import static org.junit.Assert.assertNotNull;
 
-import de.hhu.stups.plues.data.entities.Course;
-import de.hhu.stups.plues.prob.FeasibilityResult;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXException;
@@ -14,51 +11,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.IntStream;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-public class TestRenderer {
-
-  private MockStore store;
-  private Course course;
-  private FeasibilityResult result;
+public class TestRenderer extends TestBase {
 
   /**
    * Test setup.
    */
   @Before
   public void setUp() throws URISyntaxException {
-
-    store = new MockStore();
-    course = store.getCourseByKey("foo");
-
-
-    final Map<Integer, Integer> groupChoice = new HashMap<>();
-    groupChoice.put(1, 1);
-    groupChoice.put(2, 12);
-    groupChoice.put(3, 3);
-    groupChoice.put(4, 4);
-    groupChoice.put(5, 11);
-    groupChoice.put(6, 6);
-    groupChoice.put(7, 7);
-    groupChoice.put(8, 10);
-    groupChoice.put(9, 9);
-
-    final Map<Integer, Integer> semesterChoice = new HashMap<>();
-    IntStream.rangeClosed(1, 9).forEach(i -> semesterChoice.put(i, 1));
-
-    final Map<String, Set<Integer>> moduleChoice = new HashMap<>();
-    final Set<Integer> integerSet = new HashSet<Integer>();
-    integerSet.add(1);
-    integerSet.add(3);
-    moduleChoice.put("foo", integerSet);
-
-    this.result = new FeasibilityResult(moduleChoice, semesterChoice, groupChoice);
+    super.setUp();
   }
 
   @Test
