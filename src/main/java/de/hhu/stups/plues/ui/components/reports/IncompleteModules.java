76e7aead85656a152bdb055dfc3c1cdc6aaf9866
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-11-14 15:06:09 +0100

    Display title instead of id for incomplete modules

--- src/main/java/de/hhu/stups/plues/ui/components/reports/IncompleteModules.java
+++ src/main/java/de/hhu/stups/plues/ui/components/reports/IncompleteModules.java
@@ -2,6 +2,8 @@ package de.hhu.stups.plues.ui.components.reports;
 
 import com.google.inject.Inject;
 
+import de.hhu.stups.plues.Delayed;
+import de.hhu.stups.plues.data.Store;
 import de.hhu.stups.plues.ui.layout.Inflater;
 
 import java.net.URL;
@@ -13,17 +15,23 @@ import javafx.scene.control.ListView;
 
 public class IncompleteModules extends ListView implements Initializable {
 
+  private final Delayed<Store> delayedStore;
+  private Store store;
+
   @Inject
-  public IncompleteModules(final Inflater inflater) {
+  public IncompleteModules(final Inflater inflater,
+                           final Delayed<Store> delayedStore) {
+    this.delayedStore = delayedStore;
+
     inflater.inflate("/components/reports/IncompleteModules", this, this, "reports");
   }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-
+    delayedStore.whenAvailable(store -> this.store = store);
   }
 
   public void setData(final Set<Integer> incompleteModules) {
-    getItems().setAll(incompleteModules);
+    incompleteModules.forEach(id -> getItems().add(store.getModuleById(id).getTitle()));
   }
 }
