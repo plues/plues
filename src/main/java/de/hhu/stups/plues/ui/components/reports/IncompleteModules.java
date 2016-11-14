77255902998e86a608a7fcc459042e1f1b6a2ff4
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-11-14 16:39:56 +0100

    Add pordnr to incomplete modules pane

--- src/main/java/de/hhu/stups/plues/ui/components/reports/IncompleteModules.java
+++ src/main/java/de/hhu/stups/plues/ui/components/reports/IncompleteModules.java
@@ -4,16 +4,33 @@ import com.google.inject.Inject;
 
 import de.hhu.stups.plues.Delayed;
 import de.hhu.stups.plues.data.Store;
+import de.hhu.stups.plues.data.entities.Module;
 import de.hhu.stups.plues.ui.layout.Inflater;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 
-public class IncompleteModules extends ListView implements Initializable {
+public class IncompleteModules extends VBox implements Initializable {
+
+  @FXML
+  @SuppressWarnings("unused")
+  private TableView<Module> tableViewModules;
+
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Module, Integer> columnModulePordnr;
+
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Module, String> columnModuleTitle;
 
   private final Delayed<Store> delayedStore;
   private Store store;
@@ -28,10 +45,13 @@ public class IncompleteModules extends ListView implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    columnModulePordnr.setCellValueFactory(new PropertyValueFactory<>("pordnr"));
+    columnModuleTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+
     delayedStore.whenAvailable(store -> this.store = store);
   }
 
   public void setData(final Set<Integer> incompleteModules) {
-    incompleteModules.forEach(id -> getItems().add(store.getModuleById(id).getTitle()));
+    incompleteModules.forEach(id -> tableViewModules.getItems().add(store.getModuleById(id)));
   }
 }
