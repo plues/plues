23b318c85d183e74c60017a7c58dea7896559f0d
David Schneider <david.schneider@bivab.de>
2016-11-17 21:37:33 +0100

    Show courses as table of (key and fullName) and added pordnr to table of modules

--- src/main/java/de/hhu/stups/plues/ui/components/reports/MandatoryModules.java
+++ src/main/java/de/hhu/stups/plues/ui/components/reports/MandatoryModules.java
@@ -5,77 +5,83 @@ import com.google.inject.Inject;
 import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.data.entities.Module;
 import de.hhu.stups.plues.ui.layout.Inflater;
-
+import javafx.beans.binding.ListBinding;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 
 public class MandatoryModules extends VBox implements Initializable {
 
-  private Map<Course, Set<Module>> mandatoryModules;
-  private SimpleListProperty<Course> courses;
-  private SimpleListProperty<Module> modules;
+  private final ObservableMap<Course, Set<Module>> mandatoryModules;
+  private final SimpleListProperty<Course> courses;
 
   @FXML
-  @SuppressWarnings("unused")
-  private ListView<Course> listViewCourses;
+  private TableView<Course> tableViewcourses;
   @FXML
-  @SuppressWarnings("unused")
+  private TableColumn<Course, String> tableColumnCourseName;
+  @FXML
+  private TableColumn<Course, String> tableColumnCourseFullName;
+  @FXML
   private TableView<Module> tableViewMandatoryModules;
   @FXML
-  @SuppressWarnings("unused")
   private TableColumn<Module, String> columnModuleTitle;
   @FXML
-  @SuppressWarnings("unused")
   private TableColumn<Module, Boolean> columnModuleElectability;
+  @FXML
+  private TableColumn<Module, String> columnModulePordnr;
 
   /**
    * Default constructor.
+   *
    * @param inflater Handle fxml and resources
    */
   @Inject
   public MandatoryModules(final Inflater inflater) {
     courses = new SimpleListProperty<>(FXCollections.observableArrayList());
-    modules = new SimpleListProperty<>(FXCollections.observableArrayList());
-
+    mandatoryModules = FXCollections.observableHashMap();
     inflater.inflate("/components/reports/MandatoryModules", this, this, "reports");
   }
 
   @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    listViewCourses.itemsProperty().bind(courses);
-    listViewCourses.setCellFactory(param -> new ListCell<Course>() {
-      @Override
-      protected void updateItem(Course course, boolean empty) {
-        super.updateItem(course, empty);
-        if (!empty) {
-          setText(course.getKey());
-        }
-      }
-    });
-    tableViewMandatoryModules.itemsProperty().bind(modules);
+  public void initialize(final URL location, final ResourceBundle resources) {
+    tableColumnCourseName.setCellValueFactory(new PropertyValueFactory<>("key"));
+    tableColumnCourseFullName.setCellValueFactory(new PropertyValueFactory<>("fullName"));
+
     columnModuleTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
     columnModuleElectability.setCellValueFactory(new PropertyValueFactory<>("mandatory"));
+    columnModulePordnr.setCellValueFactory(new PropertyValueFactory<>("pordnr"));
 
-    listViewCourses.getSelectionModel().selectedItemProperty()
-        .addListener((observable, oldValue, newValue) ->
-          modules.setAll(mandatoryModules.get(newValue)));
+    tableViewcourses.itemsProperty().bind(courses);
+    tableViewMandatoryModules.itemsProperty().bind(new ListBinding<Module>() {
+      {
+        bind(tableViewcourses.getSelectionModel().selectedItemProperty());
+      }
+
+      @Override
+      protected ObservableList<Module> computeValue() {
+        final Course course =  tableViewcourses.getSelectionModel().getSelectedItem();
+        return FXCollections.observableArrayList(
+            mandatoryModules.getOrDefault(course, Collections.emptySet()));
+      }
+    });
+
   }
 
   public void setData(final Map<Course, Set<Module>> mandatoryModules) {
-    this.mandatoryModules = mandatoryModules;
+    this.mandatoryModules.putAll(mandatoryModules);
     courses.addAll(mandatoryModules.keySet());
   }
 }
