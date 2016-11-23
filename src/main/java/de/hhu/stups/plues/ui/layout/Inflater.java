a1c291da19ba320a7deac74f00dcc78aab743f30
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-11-23 13:25:32 +0100

    Add custom resource bundle to handle multiple bundle needs. Creates a intersection of all required and main for all as default (This closes #70)

--- src/main/java/de/hhu/stups/plues/ui/layout/Inflater.java
+++ src/main/java/de/hhu/stups/plues/ui/layout/Inflater.java
@@ -8,10 +8,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Inflates a layout from a filename.
@@ -19,7 +25,8 @@ import java.util.logging.Logger;
 public class Inflater {
 
   private final FXMLLoader loader;
-  private static final String DEFAULT_BUNDLE = "main";
+  private static final Locale LOCALE = Locale.getDefault();
+  private static final ResourceBundle MAIN_BUNDLE = ResourceBundle.getBundle("lang.main", LOCALE);
 
   @Inject
   public Inflater(final FXMLLoader loader) {
@@ -30,11 +37,11 @@ public class Inflater {
    * Inflate a fxml resource as a layout from <tt>/fxml/</tt>.
    *
    * @param name The name of the xml file without the <tt>.fxml</tt> extension.
-   * @param bundleName The name of the used bundle
+   * @param bundleNames The name of the used bundles
    * @return {@link Parent}
    */
-  public Parent inflate(final String name, final String bundleName) {
-    return inflate(name, null, null, bundleName);
+  public Parent inflate(final String name, final String... bundleNames) {
+    return inflate(name, null, null, bundleNames);
   }
 
   /**
@@ -43,7 +50,7 @@ public class Inflater {
    * @return {@link Parent}
    */
   public Parent inflate(final String name) {
-    return inflate(name, null, null, DEFAULT_BUNDLE);
+    return inflate(name, null, null, new String[0]);
   }
 
   /**
@@ -54,7 +61,7 @@ public class Inflater {
    * @return {@link Parent}
    */
   public Parent inflate(final String name, final Parent root, final Object controller) {
-    return inflate(name, root, controller, DEFAULT_BUNDLE);
+    return inflate(name, root, controller, new String[0]);
   }
 
   /**
@@ -63,10 +70,10 @@ public class Inflater {
    * @param name The name of the fxml file without the <tt>.fxml</tt> extension.
    * @param root optional root node to inflate this layout into
    * @param controller controller for the fxml file
-   * @param bundleName Name of the i18n resource to bind
+   * @param bundleNames Name of the i18n resources to bind
    */
   public Parent inflate(final String name, final Parent root,
-                        final Object controller, final String bundleName) {
+                        final Object controller, final String... bundleNames) {
     // set location explicitly to ensure using the injected fxml loader
     loader.setLocation(getClass().getResource("/fxml/" + name + ".fxml"));
 
@@ -78,10 +85,13 @@ public class Inflater {
       loader.setController(controller);
     }
 
+    ResourceBundle[] bundles = new ResourceBundle[bundleNames.length + 1];
+    for (int i = 0;i < bundleNames.length; i++) {
+      bundles[i] = ResourceBundle.getBundle("lang." + bundleNames[i], LOCALE);
+    }
+    bundles[bundleNames.length] = MAIN_BUNDLE;
 
-    final ResourceBundle bundle = ResourceBundle.getBundle("lang." + bundleName,
-        Locale.getDefault());
-    loader.setResources(bundle);
+    loader.setResources(new CustomMultiResourceBundle(bundles));
 
     try {
       return loader.load();
@@ -89,6 +99,37 @@ public class Inflater {
       final Logger logger = Logger.getLogger(getClass().getSimpleName());
       logger.log(Level.SEVERE, "Exception in FXML Loader", ignored);
       throw new InflaterException(ignored);
+    }
+  }
+
+  private final class CustomMultiResourceBundle extends ResourceBundle {
+
+    private final ResourceBundle[] resourceBundles;
+
+    public CustomMultiResourceBundle(final ResourceBundle... resourceBundles) {
+      this.resourceBundles = resourceBundles;
+    }
+
+    @Override
+    protected Object handleGetObject(String key) {
+      for (ResourceBundle resourceBundle : resourceBundles) {
+        if (!resourceBundle.containsKey(key)) {
+          continue;
+        }
+        return resourceBundle.getString(key);
+      }
+
+      return null;
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+      Set<String> allKeys = new HashSet<>();
+      Set<Set<String>> keySets =
+          Arrays.stream(resourceBundles).map(ResourceBundle::keySet).collect(Collectors.toSet());
+      keySets.forEach(allKeys::addAll);
+
+      return Collections.enumeration(allKeys);
     }
   }
 }
