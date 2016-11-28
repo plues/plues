81ddc53022f04d2c93e52c9b7b40c6f42d2e369a
David Schneider <david.schneider@bivab.de>
2016-11-28 14:52:27 +0100

    Added more task state symbols and colors

--- src/main/java/de/hhu/stups/plues/ui/TaskStateColor.java
+++ src/main/java/de/hhu/stups/plues/ui/TaskStateColor.java
@@ -1,7 +1,8 @@
 package de.hhu.stups.plues.ui;
 
 public enum TaskStateColor {
-  WARNING("#FEEFB3"), FAILURE("#FFBABA"), SUCCESS("#DFF2BF"), WORKING("#BDE5F8");
+  WARNING("#FEEFB3"), FAILURE("#FFBABA"), SUCCESS("#DFF2BF"),
+  WORKING("#BDE5F8"), READY("#FFFCE6"), SCHEDULED("#FFFCE6");
 
   private final String color;
 
