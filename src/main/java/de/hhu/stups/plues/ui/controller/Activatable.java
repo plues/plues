029d46aaa36453403ff04a4cdf634e75fd226421
schmidt <joshua.schmidt@uni-duesseldorf.de>
2016-12-02 10:25:33 +0100

    add the interface Activatable for controllers that can be accessed via the router and need to activate themselves

--- src/main/java/de/hhu/stups/plues/ui/controller/Activatable.java
+++ src/main/java/de/hhu/stups/plues/ui/controller/Activatable.java
@@ -4,7 +4,8 @@ import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.routes.Router;
 
 /**
- * Interface for controllers that are called from other components via the {@link Router}.
+ * Interface for controllers that are called from other components via the {@link Router} and need
+ * to activate themselves in any way.
  */
 @FunctionalInterface
 public interface Activatable {
