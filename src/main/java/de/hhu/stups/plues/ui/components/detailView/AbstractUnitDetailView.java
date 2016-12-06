70dce74d2589ead3ff842018be6bd5e731cba51c
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-12-06 10:07:51 +0100

    Empty new detailView controller

---
+++ src/main/java/de/hhu/stups/plues/ui/components/detailView/AbstractUnitDetailView.java
@@ -0,0 +1,24 @@
+package de.hhu.stups.plues.ui.components.detailView;
+
+import com.google.inject.Inject;
+
+import de.hhu.stups.plues.ui.layout.Inflater;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AbstractUnitDetailView extends VBox implements Initializable {
+
+  @Inject
+  public AbstractUnitDetailView(final Inflater inflater) {
+    inflater.inflate("components/detailView/AbstractUnitDetailView", this, this, "detailView");
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+}
