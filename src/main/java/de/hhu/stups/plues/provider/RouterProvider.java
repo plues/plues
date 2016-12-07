d93b36e2fbe6c1657c04cd7945a7c096b4aac753
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-12-07 11:35:02 +0100

    Add new routes for detailViews to router provider

--- src/main/java/de/hhu/stups/plues/provider/RouterProvider.java
+++ src/main/java/de/hhu/stups/plues/provider/RouterProvider.java
@@ -3,22 +3,35 @@ package de.hhu.stups.plues.provider;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
+import de.hhu.stups.plues.routes.AbstractUnitDetailViewRoute;
 import de.hhu.stups.plues.routes.IndexRoute;
+import de.hhu.stups.plues.routes.ModuleDetailViewRoute;
+import de.hhu.stups.plues.routes.RouteNames;
 import de.hhu.stups.plues.routes.Router;
-import de.hhu.stups.plues.ui.layout.Inflater;
-
-import javafx.stage.Stage;
+import de.hhu.stups.plues.routes.SessionDetailViewRoute;
+import de.hhu.stups.plues.routes.UnitDetailViewRoute;
 
 public class RouterProvider implements Provider<Router> {
+  private final Provider<IndexRoute> indexRouteProvider;
   private Router cache;
 
-  private final Inflater inflater;
-  private final Stage stage;
+  private final Provider<ModuleDetailViewRoute> moduleDetailViewProvider;
+  private Provider<AbstractUnitDetailViewRoute> abstractUnitDetailViewProvider;
+  private Provider<UnitDetailViewRoute> unitDetailViewProvider;
+  private Provider<SessionDetailViewRoute> sessionDetailViewProvider;
 
   @Inject
-  public RouterProvider(Inflater inflater, Stage stage) {
-    this.inflater = inflater;
-    this.stage = stage;
+  public RouterProvider(final Provider<IndexRoute> indexRouteProvider,
+                        final Provider<ModuleDetailViewRoute> moduleDetailViewProvider,
+                        final Provider<AbstractUnitDetailViewRoute>
+                            abstractUnitDetailViewRouteProvider,
+                        final Provider<UnitDetailViewRoute> unitDetailViewRouteProvider,
+                        final Provider<SessionDetailViewRoute> sessionDetailViewRouteProvider) {
+    this.indexRouteProvider = indexRouteProvider;
+    this.moduleDetailViewProvider = moduleDetailViewProvider;
+    this.abstractUnitDetailViewProvider = abstractUnitDetailViewRouteProvider;
+    this.unitDetailViewProvider = unitDetailViewRouteProvider;
+    this.sessionDetailViewProvider = sessionDetailViewRouteProvider;
   }
 
   @Override
@@ -26,7 +39,12 @@ public class RouterProvider implements Provider<Router> {
     if (cache == null) {
       cache = new Router();
 
-      cache.put("index", new IndexRoute(inflater, stage));
+      cache.put(RouteNames.INDEX.getRouteName(), indexRouteProvider.get());
+      cache.put(RouteNames.MODULE_DETAIL_VIEW.getRouteName(), moduleDetailViewProvider.get());
+      cache.put(RouteNames.SESSION_DETAIL_VIEW.getRouteName(), sessionDetailViewProvider.get());
+      cache.put(RouteNames.ABSTRACT_UNIT_DETAIL_VIEW.getRouteName(),
+          abstractUnitDetailViewProvider.get());
+      cache.put(RouteNames.UNIT_DETAIL_VIEW.getRouteName(), unitDetailViewProvider.get());
     }
 
     return cache;
