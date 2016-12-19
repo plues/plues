5e73cdde6d2e05795b69e50463b91d782f6f7749
David Schneider <david.schneider@bivab.de>
2016-12-19 14:12:55 +0100

    Added helper methods to check the some of the different ResultStates

--- src/main/java/de/hhu/stups/plues/prob/ResultState.java
+++ src/main/java/de/hhu/stups/plues/prob/ResultState.java
@@ -1,5 +1,21 @@
 package de.hhu.stups.plues.prob;
 
 public enum ResultState {
-  FAILED, SUCCEEDED, INTERRUPTED, TIMEOUT, IMPOSSIBLE, IMPOSSIBLE_COMBINATION
+  FAILED, SUCCEEDED, INTERRUPTED, TIMEOUT, IMPOSSIBLE, IMPOSSIBLE_COMBINATION;
+
+  public Boolean failed() {
+    return this.equals(FAILED);
+  }
+
+  public Boolean succeeded() {
+    return this.equals(SUCCEEDED);
+  }
+
+  public boolean timedOut() {
+    return this.equals(TIMEOUT);
+  }
+
+  public boolean isImpossible() {
+    return this.equals(IMPOSSIBLE);
+  }
 }
