4b5ea26db4750b5ce5db4b3a6a704b0500ae088d
David Schneider <david.schneider@bivab.de>
2016-12-24 16:29:48 +0100

    Extracted all event Handlers to custom methods

--- src/main/java/de/hhu/stups/plues/ui/controller/unsatcore/UnsatCore.java
+++ src/main/java/de/hhu/stups/plues/ui/controller/unsatcore/UnsatCore.java
@@ -15,7 +15,6 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
@@ -31,7 +30,6 @@ public class UnsatCore extends VBox implements Initializable, Activatable {
   private final ObjectProperty<SolverService> solverService;
   private final ObjectProperty<Store> store;
   private final ExecutorService executorService;
-  private ResourceBundle resources;
 
   @FXML
   private CourseUnsatCore courseUnsatCore;
@@ -65,64 +63,28 @@ public class UnsatCore extends VBox implements Initializable, Activatable {
 
   @Override
   public void initialize(final URL location, final ResourceBundle resources) {
-    this.resources = resources;
-
-    configureCourseUnsatCore(resources);
-    configureModuleUnsatCore(resources);
-    configureAbstractUnitUnsatCore(resources);
-    configureGroupUnsatCore(resources);
+    initializeCourseUnsatCore();
+    initializeModuleUnsatCore();
+    initializeAbstractUnitUnsatCore();
+    initializeGroupUnsatCore();
   }
 
-  private void configureCourseUnsatCore(final ResourceBundle resources) {
+  private void initializeCourseUnsatCore() {
     courseUnsatCore.coursesProperty().addListener((observable, oldValue, newValue) -> {
       moduleUnsatCore.setModules(FXCollections.emptyObservableList());
       moduleUnsatCore.resetTaskState();
     });
 
     final BooleanBinding binding = solverService.isNull()
-      .or(courseUnsatCore.coursesProperty().emptyProperty())
-      .or(moduleUnsatCore.getModuleProperty().emptyProperty().not());
+        .or(courseUnsatCore.coursesProperty().emptyProperty())
+        .or(moduleUnsatCore.getModuleProperty().emptyProperty().not());
 
     final UnsatCoreButtonBar unsatCoreButtonBar = courseUnsatCore.getUnsatCoreButtonBar();
     unsatCoreButtonBar.disableProperty().bind(binding);
     unsatCoreButtonBar.setOnAction(this::computeUnsatCoreModules);
   }
 
-  private void computeUnsatCoreModules(final ActionEvent event) {
-    final ObservableList<Course> courseList = courseUnsatCore.coursesProperty().get();
-    final Course[] selectedCourses = new Course[courseList.size()];
-    final SolverTask<Set<Integer>> task =
-      getSolverService().unsatCoreModules(courseList.toArray(selectedCourses));
-
-    task.setOnSucceeded(succeeded -> {
-      final Set<Integer> moduleIds = task.getValue();
-      moduleUnsatCore.setModules(moduleIds.stream().map(getStore()::getModuleById)
-        .collect(Collectors.collectingAndThen(Collectors.toList(),
-          FXCollections::observableArrayList)));
-    });
-
-    courseUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
-
-    executorService.submit(task);
-  }
-
-  private void configureModuleUnsatCore(final ResourceBundle resources) {
-    final EventHandler<ActionEvent> eventHandler = event -> {
-      final SolverTask<Set<Integer>> task =
-        getSolverService().unsatCoreAbstractUnits(moduleUnsatCore.getModuleProperty().get());
-
-      task.setOnSucceeded(succeeded -> {
-        final Set<Integer> abstractUnitIds = task.getValue();
-        abstractUnitUnsatCore.setAbstractUnits(abstractUnitIds.stream()
-          .map(getStore()::getAbstractUnitById).collect(Collectors
-            .collectingAndThen(Collectors.toList(), FXCollections::observableArrayList)));
-
-      });
-
-      moduleUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
-      executorService.submit(task);
-    };
-
+  private void initializeModuleUnsatCore() {
     moduleUnsatCore.getModuleProperty().addListener((observable, oldValue, newValue) -> {
       abstractUnitUnsatCore.setAbstractUnits(FXCollections.emptyObservableList());
       abstractUnitUnsatCore.resetTaskState();
@@ -130,68 +92,106 @@ public class UnsatCore extends VBox implements Initializable, Activatable {
 
 
     final BooleanBinding binding = moduleUnsatCore.getModuleProperty().emptyProperty()
-      .or(abstractUnitUnsatCore.getAbstractUnits().emptyProperty().not());
+        .or(abstractUnitUnsatCore.getAbstractUnits().emptyProperty().not());
 
     final UnsatCoreButtonBar unsatCoreButtonBar = moduleUnsatCore.getUnsatCoreButtonBar();
     unsatCoreButtonBar.disableProperty().bind(binding);
-    unsatCoreButtonBar.setOnAction(eventHandler);
+    unsatCoreButtonBar.setOnAction(this::computeUnsatCoreAbstractUnits);
   }
 
-  private void configureAbstractUnitUnsatCore(final ResourceBundle resources) {
-    final EventHandler<ActionEvent> eventHandler = event -> {
-      final SolverTask<Set<Integer>> task =
-        getSolverService().unsatCoreGroups(abstractUnitUnsatCore.getAbstractUnits().get(),
-          moduleUnsatCore.getModuleProperty().get());
-
-      task.setOnSucceeded(succeeded -> {
-        final Set<Integer> groupIds = task.getValue();
-        groupUnsatCore.setGroups(groupIds.stream().map(getStore()::getGroupById).collect(Collectors
-          .collectingAndThen(Collectors.toList(), FXCollections::observableArrayList)));
-      });
-
-      abstractUnitUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
-      executorService.submit(task);
-    };
-
+  private void initializeAbstractUnitUnsatCore() {
     abstractUnitUnsatCore.getAbstractUnits().addListener((observable, oldValue, newValue) -> {
       groupUnsatCore.setGroups(FXCollections.emptyObservableList());
       groupUnsatCore.resetTaskState();
     });
 
-
     final BooleanBinding binding = abstractUnitUnsatCore.getAbstractUnits().emptyProperty()
-      .or(groupUnsatCore.getGroupProperty().emptyProperty().not());
+        .or(groupUnsatCore.getGroupProperty().emptyProperty().not());
 
     final UnsatCoreButtonBar unsatCoreButtonBar = abstractUnitUnsatCore.getUnsatCoreButtonBar();
     unsatCoreButtonBar.disableProperty().bind(binding);
-    unsatCoreButtonBar.setOnAction(eventHandler);
+    unsatCoreButtonBar.setOnAction(this::computeUnsatCoreGroups);
   }
 
-  private void configureGroupUnsatCore(final ResourceBundle resources) {
-    final EventHandler<ActionEvent> eventHandler = event -> {
-      final SolverTask<Set<Integer>> task = getSolverService().unsatCoreSessions(
-        groupUnsatCore.getGroupProperty().get());
-
-      task.setOnSucceeded(succeeded -> {
-        final Set<Integer> sessionIds = task.getValue();
-        sessionUnsatCore.setSessions(sessionIds.stream().map(getStore()::getSessionById).collect(
-          Collectors.collectingAndThen(Collectors.toList(), FXCollections::observableArrayList)));
-      });
-
-      groupUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
-      executorService.submit(task);
-    };
-
+  private void initializeGroupUnsatCore() {
     groupUnsatCore.getGroupProperty().addListener((observable, oldValue, newValue) ->
-      sessionUnsatCore.setSessions(FXCollections.emptyObservableList()));
+        sessionUnsatCore.setSessions(FXCollections.emptyObservableList()));
 
 
     final BooleanBinding binding = groupUnsatCore.getGroupProperty().emptyProperty()
-      .or(sessionUnsatCore.getSessionProperty().emptyProperty().not());
+        .or(sessionUnsatCore.getSessionProperty().emptyProperty().not());
 
     final UnsatCoreButtonBar unsatCoreButtonBar = groupUnsatCore.getUnsatCoreButtonBar();
     unsatCoreButtonBar.disableProperty().bind(binding);
-    unsatCoreButtonBar.setOnAction(eventHandler);
+    unsatCoreButtonBar.setOnAction(this::computeUnsatCoreSessions);
+  }
+
+  @SuppressWarnings("unused")
+  private void computeUnsatCoreGroups(final ActionEvent actionEvent) {
+    final SolverTask<Set<Integer>> task
+        = getSolverService().unsatCoreGroups(
+          abstractUnitUnsatCore.getAbstractUnits().get(),
+          moduleUnsatCore.getModuleProperty().get());
+
+    task.setOnSucceeded(succeeded -> {
+      final Set<Integer> groupIds = task.getValue();
+      groupUnsatCore.setGroups(groupIds.stream().map(getStore()::getGroupById).collect(Collectors
+          .collectingAndThen(Collectors.toList(), FXCollections::observableArrayList)));
+    });
+
+    abstractUnitUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
+    executorService.submit(task);
+  }
+
+  @SuppressWarnings("unused")
+  private void computeUnsatCoreModules(final ActionEvent event) {
+    final ObservableList<Course> courseList = courseUnsatCore.coursesProperty().get();
+    final Course[] selectedCourses = new Course[courseList.size()];
+    final SolverTask<Set<Integer>> task
+        = getSolverService().unsatCoreModules(courseList.toArray(selectedCourses));
+
+    task.setOnSucceeded(succeeded -> {
+      final Set<Integer> moduleIds = task.getValue();
+      moduleUnsatCore.setModules(moduleIds.stream().map(getStore()::getModuleById)
+          .collect(
+            Collectors.collectingAndThen(Collectors.toList(), FXCollections::observableArrayList)));
+    });
+
+    courseUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
+
+    executorService.submit(task);
+  }
+
+  @SuppressWarnings("unused")
+  private void computeUnsatCoreAbstractUnits(final ActionEvent actionEvent) {
+    final SolverTask<Set<Integer>> task
+        = getSolverService().unsatCoreAbstractUnits(moduleUnsatCore.getModuleProperty().get());
+
+    task.setOnSucceeded(succeeded -> {
+      final Set<Integer> abstractUnitIds = task.getValue();
+      abstractUnitUnsatCore.setAbstractUnits(abstractUnitIds.stream()
+          .map(getStore()::getAbstractUnitById).collect(Collectors
+            .collectingAndThen(Collectors.toList(), FXCollections::observableArrayList)));
+
+    });
+
+    moduleUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
+    executorService.submit(task);
+  }
+
+  @SuppressWarnings("unused")
+  private void computeUnsatCoreSessions(final ActionEvent actionEvent) {
+    final SolverTask<Set<Integer>> task
+        = getSolverService().unsatCoreSessions(groupUnsatCore.getGroupProperty().get());
+
+    task.setOnSucceeded(succeeded -> {
+      final Set<Integer> sessionIds = task.getValue();
+      sessionUnsatCore.setSessions(sessionIds.stream().map(getStore()::getSessionById).collect(
+          Collectors.collectingAndThen(Collectors.toList(), FXCollections::observableArrayList)));
+    });
+
+    groupUnsatCore.getUnsatCoreButtonBar().showTaskState(task);
+    executorService.submit(task);
   }
 
   public SolverService getSolverService() {
