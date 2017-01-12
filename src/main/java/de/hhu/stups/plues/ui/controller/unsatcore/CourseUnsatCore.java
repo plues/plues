d68743496402a91ffb4d35cbf1b13e8fdb7fa900
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-01-12 14:05:39 +0100

    add icon with tooltip displaying the component's explanation instead of showing the whole text all the time

--- src/main/java/de/hhu/stups/plues/ui/controller/unsatcore/CourseUnsatCore.java
+++ src/main/java/de/hhu/stups/plues/ui/controller/unsatcore/CourseUnsatCore.java
@@ -8,7 +8,10 @@ import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.services.UiDataService;
 import de.hhu.stups.plues.ui.components.CombinationOrSingleCourseSelection;
 import de.hhu.stups.plues.ui.layout.Inflater;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleListProperty;
@@ -16,17 +19,23 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CourseUnsatCore extends VBox implements Initializable {
+public class CourseUnsatCore extends GridPane implements Initializable {
 
   private final ObjectProperty<Store> storeProperty;
   private final ListProperty<Course> coursesProperty;
   private final UiDataService uiDataService;
 
+  @FXML
+  @SuppressWarnings("unused")
+  private Label unsatCoreInfo;
   @FXML
   @SuppressWarnings("unused")
   private CombinationOrSingleCourseSelection courseSelection;
@@ -38,8 +47,7 @@ public class CourseUnsatCore extends VBox implements Initializable {
    * Initialize the component.
    */
   @Inject
-  public CourseUnsatCore(final Inflater inflater,
-                         final Delayed<Store> delayedStore,
+  public CourseUnsatCore(final Inflater inflater, final Delayed<Store> delayedStore,
                          final UiDataService uiDataService) {
     this.uiDataService = uiDataService;
     this.storeProperty = new SimpleObjectProperty<>();
@@ -56,6 +64,20 @@ public class CourseUnsatCore extends VBox implements Initializable {
 
   @Override
   public void initialize(final URL location, final ResourceBundle resources) {
+    final Tooltip infoTooltip = new Tooltip(resources.getString("unsatCoreExplanation"));
+    infoTooltip.setPrefWidth(400.0);
+    infoTooltip.setWrapText(true);
+
+    unsatCoreInfo.setOnMouseEntered(event -> {
+      final Point2D pos = unsatCoreInfo.localToScreen(
+          unsatCoreInfo.getLayoutBounds().getMaxX(), unsatCoreInfo.getLayoutBounds().getMaxY());
+      infoTooltip.show(unsatCoreInfo, pos.getX(), pos.getY());
+    });
+    unsatCoreInfo.setOnMouseExited(event -> infoTooltip.hide());
+
+    unsatCoreInfo.graphicProperty().bind(Bindings.createObjectBinding(() ->
+        FontAwesomeIconFactory.get().createIcon(FontAwesomeIcon.INFO_CIRCLE, "20")));
+
     storeProperty.addListener((observable, oldValue, store)
         -> courseSelection.setCourses(store.getCourses()));
 
