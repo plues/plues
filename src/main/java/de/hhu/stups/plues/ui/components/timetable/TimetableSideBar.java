7a35d2203f5ebeac325ad4438d43dc3f794067be
David Schneider <david.schneider@bivab.de>
2017-01-17 08:06:31 +0100

    Handle clicks anywhere in tab headers. (i.e. on the close button)
    Closes #153

--- src/main/java/de/hhu/stups/plues/ui/components/timetable/TimetableSideBar.java
+++ src/main/java/de/hhu/stups/plues/ui/components/timetable/TimetableSideBar.java
@@ -11,24 +11,21 @@ import de.hhu.stups.plues.ui.components.CheckCourseFeasibility;
 import de.hhu.stups.plues.ui.components.SetOfCourseSelection;
 import de.hhu.stups.plues.ui.controller.Timetable;
 import de.hhu.stups.plues.ui.layout.Inflater;
-import javafx.collections.ObservableList;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.Event;
-import javafx.event.EventTarget;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.Arrays;
-import java.util.Optional;
 import java.util.ResourceBundle;
 
 
@@ -37,7 +34,7 @@ public class TimetableSideBar extends TabPane implements Initializable {
   private static final String SIDE_BAR_TAB_LAYOUT = "sideBarTabLayout";
 
   private final UiDataService uiDataService;
-  private Tab selectedSubTab;
+  private Node selectedSubTab;
   private Timetable parent;
 
   @FXML
@@ -56,6 +53,8 @@ public class TimetableSideBar extends TabPane implements Initializable {
   @SuppressWarnings("unused")
   private CheckCourseFeasibility checkCourseFeasibility;
 
+  private BooleanProperty collapsed = new SimpleBooleanProperty(false);
+
   @Inject
   public TimetableSideBar(final Inflater inflater, final UiDataService uiDataService) {
     this.uiDataService = uiDataService;
@@ -71,21 +70,70 @@ public class TimetableSideBar extends TabPane implements Initializable {
       sideTab.setOnCloseRequest(Event::consume);
     });
 
-    selectedSubTab = getSelectionModel().getSelectedItem();
+    this.collapsed.addListener((observable, oldValue, shouldHide) -> {
+      if (shouldHide) {
+        hideSideBar();
+      } else {
+        showSideBar();
+      }
+    });
 
-    addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-      final EventTarget eventTarget = mouseEvent.getTarget();
-      if (eventTarget instanceof Text) {
-        handleSideBarTabs(((Text) eventTarget).getText());
-      } else if (eventTarget instanceof StackPane) {
-        final ObservableList<String> styleClasses = ((StackPane) eventTarget).getStyleClass();
-        if (styleClasses.contains("tab-container")) {
-          final Optional<Node> optionalLabel = ((StackPane) eventTarget).getChildren().stream()
-              .filter(node -> node instanceof Label).findFirst();
-          optionalLabel.ifPresent(node -> handleSideBarTabs(((Label) node).getText()));
+    addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+      final Node clickedTab = getClickedTab(mouseEvent);
+      if (clickedTab != null) {
+        if (selectedSubTab == null) {
+          selectedSubTab = this.lookup(".tab:first-child");
+        }
+        if (selectedSubTab.equals(clickedTab)) { // switch state
+          mouseEvent.consume();
+          this.collapsed.set(!this.collapsed.get());
+        } else { // switch tab and make visible
+          selectedSubTab = clickedTab;
+          this.collapsed.set(false);
         }
       }
     });
+  }
+
+  /**
+   * Find the tab that was clicked in the mouse event.
+   * @param mouseEvent MouseEvent for the click
+   * @return Node representing the clicked tab, null if the click was not on a tab header.
+   */
+  private Node getClickedTab(final MouseEvent mouseEvent) {
+    Node node = (Node) mouseEvent.getTarget();
+
+    if (!inTabHeader(node)) {
+      return null;
+    }
+
+    do {
+      if (!node.getStyleClass().contains("tab")) {
+        continue;
+      }
+      return node;
+    }
+    while ((node = node.getParent()) != null);
+
+    return null;
+  }
+
+  /**
+   * Check if the node is in a tab header.
+   * @param root Node to check
+   * @return boolean
+   */
+  private boolean inTabHeader(final Node root) {
+    Node node = root;
+    do {
+      if (!node.getStyleClass().contains("tab-container")) {
+        continue;
+      }
+      return true;
+    }
+    while ((node = node.getParent()) != null);
+
+    return false;
   }
 
   /**
@@ -134,47 +182,27 @@ public class TimetableSideBar extends TabPane implements Initializable {
     }
   }
 
-  /**
-   * Identify each tab by its text and handle the visibility of the side bar according to the
-   * selected tab's state.
-   */
-  @SuppressWarnings("unused")
-  private void handleSideBarTabs(final String tabText) {
-    final Optional<Tab> optionalTab = getTabs().stream()
-        .filter(tab -> tab.getText().equals(tabText)).findFirst();
-    optionalTab.ifPresent(this::showOrHideSideBar);
+  private void hideSideBar() {
+    getSelectionModel().select(null);
+
+    parent.setDividerPosition(0, getMinWidth() / parent.getWidth());
+    parent.disableDivider(true);
+    getTabs().forEach(sideTab -> {
+      sideTab.getStyleClass().clear();
+      sideTab.getStyleClass().addAll(SIDE_BAR_TAB_LAYOUT, "tab", "sideBarTabHidden");
+    });
+    this.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+    this.getSelectionModel().select(null);
   }
 
-  /**
-   * Either show or hide the tab pane's content by moving the {@link #parent split pane's} slider.
-   * The content is hidden if a selected tab is clicked again.
-   */
-  private void showOrHideSideBar(final Tab tab) {
-    if (tab.equals(selectedSubTab)) {
-      parent.setDividerPosition(0, getMinWidth() / parent.getWidth());
-      parent.disableDivider(true);
-      selectedSubTab = null;
-      tab.getStyleClass().clear();
-      getTabs().forEach(sideTab -> {
-        sideTab.getStyleClass().clear();
-        sideTab.getStyleClass().addAll(SIDE_BAR_TAB_LAYOUT, "tab", "sideBarTabHidden");
-      });
-    } else {
-      showSideBar(tab);
-    }
-  }
-
-  private void showSideBar(final Tab tab) {
-    getSelectionModel().select(getTabs().indexOf(tab));
-    if (selectedSubTab == null) {
-      parent.restoreUserDefinedDividerPos();
-    }
+  private void showSideBar() {
+    parent.restoreUserDefinedDividerPos();
     parent.disableDivider(false);
-    selectedSubTab = tab;
     getTabs().forEach(sideTab -> {
       sideTab.getStyleClass().clear();
       sideTab.getStyleClass().addAll(SIDE_BAR_TAB_LAYOUT, "tab", "sideBarTabVisible");
     });
+    this.setTabClosingPolicy(TabClosingPolicy.SELECTED_TAB);
   }
 
   /**
@@ -196,7 +224,7 @@ public class TimetableSideBar extends TabPane implements Initializable {
 
   private void selectSideBarTab(final Tab tab) {
     getSelectionModel().select(getTabs().indexOf(tab));
-    showSideBar(tab);
+    showSideBar();
   }
 
   public SetOfCourseSelection getSetOfCourseSelection() {
