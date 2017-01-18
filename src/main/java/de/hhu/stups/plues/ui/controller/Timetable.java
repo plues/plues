5d120c5d3aac5fab9f8fe4b9ea5e7348e6d533a5
David Schneider <david.schneider@bivab.de>
2017-01-18 15:58:56 +0100

    Replaced SessionFacadeListBinding with three nested bindings.
    Goal is to reduce the number of computations performed when a filter or semester is chosen/changed.
    
    First filter filters by slot and only depends on the list of session facades
    Second filter filters by semester and depends on the previous filter and the toggle
    The last filter applies the course and abstract unit filter to the result of the second binding

--- src/main/java/de/hhu/stups/plues/ui/controller/Timetable.java
+++ src/main/java/de/hhu/stups/plues/ui/controller/Timetable.java
@@ -20,7 +20,6 @@ import de.hhu.stups.plues.ui.components.timetable.SessionListView;
 import de.hhu.stups.plues.ui.components.timetable.SessionListViewFactory;
 import de.hhu.stups.plues.ui.components.timetable.TimetableSideBar;
 import de.hhu.stups.plues.ui.layout.Inflater;
-
 import javafx.beans.Observable;
 import javafx.beans.binding.ListBinding;
 import javafx.beans.binding.SetBinding;
@@ -39,7 +38,6 @@ import javafx.scene.control.SplitPane;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.GridPane;
-
 import org.controlsfx.control.SegmentedButton;
 
 import java.net.URL;
@@ -162,7 +160,12 @@ public class Timetable extends SplitPane implements Initializable, Activatable {
     final SessionListView view = sessionListViewFactory.create(slot);
 
     view.setSessions(sessions);
-    view.itemsProperty().bind(new SessionFacadeListBinding(slot));
+
+    final ListBinding<SessionFacade> slotBinding = new SlotBinding(slot);
+    final ListBinding<SessionFacade> semesterBinding = new SemesterBinding(slotBinding);
+    final ListBinding<SessionFacade> filterBinding = new FilterBinding(semesterBinding);
+
+    view.itemsProperty().bind(filterBinding);
     return view;
   }
 
@@ -239,92 +242,6 @@ public class Timetable extends SplitPane implements Initializable, Activatable {
     getDividers().get(0).setPosition(userDefinedDividerPos);
   }
 
-  private class SessionFacadeListBinding extends ListBinding<SessionFacade> {
-
-    private final SessionFacade.Slot slot;
-
-    SessionFacadeListBinding(final SessionFacade.Slot slot) {
-      this.slot = slot;
-
-      bind(sessions, semesterToggle.getToggleGroup().selectedToggleProperty(),
-          timetableSideBar.getSetOfCourseSelection().selectedCoursesProperty(),
-          timetableSideBar.getAbstractUnitFilter().selectedAbstractUnitsProperty(),
-          uiDataService.conflictMarkedSessionsProperty());
-    }
-
-    @Override
-    protected ObservableList<SessionFacade> computeValue() {
-      final Integer semester = getSelectedSemester();
-
-      return sessions.filtered(session -> sessionIsIncluded(session, semester));
-    }
-
-    private boolean sessionIsIncluded(final SessionFacade session, final Integer semester) {
-      final boolean isIncludedBySlot = session.getSlot().equals(slot);
-      
-      return isIncludedBySlot && isIncludedBySemester(session, semester) && isNotExcluded(session);
-    }
-
-    private boolean isIncludedBySemester(final SessionFacade session, final Integer semester) {
-      final Set<Integer> semesters = session.getUnitSemesters();
-      return semester == null || semesters.contains(semester);
-    }
-
-    private boolean isNotExcluded(final SessionFacade session) {
-      return sessionIsNotExcluded(session) || sessionIsIncludedByConflict(session);
-    }
-
-    private Integer getSelectedSemester() {
-      final ToggleButton semesterButton =
-          (ToggleButton) semesterToggle.getToggleGroup().getSelectedToggle();
-
-      if (null != semesterButton) {
-        return Integer.valueOf((String) semesterButton.getUserData());
-      }
-
-      return null;
-    }
-
-    private boolean sessionIsNotExcluded(final SessionFacade session) {
-      return !sessionIsExcludedByCourse(session) && !sessionIsExcludedByAbstractUnit(session);
-    }
-
-    private boolean sessionIsIncludedByConflict(final SessionFacade session) {
-      return uiDataService.conflictMarkedSessionsProperty().stream()
-          .anyMatch(sessionId -> sessionId == session.getId());
-    }
-
-    private boolean sessionIsExcludedByCourse(final SessionFacade session) {
-      final Set<Course> filteredCourses =
-          new HashSet<>(timetableSideBar.getSetOfCourseSelection().getSelectedCourses());
-
-      if (filteredCourses.isEmpty()) {
-        return false;
-      }
-
-      final Set<Course> sessionCourses = session.getIntendedCourses();
-
-      sessionCourses.retainAll(filteredCourses);
-
-      return !filteredCourses.isEmpty() && sessionCourses.isEmpty();
-    }
-
-    private boolean sessionIsExcludedByAbstractUnit(final SessionFacade session) {
-      final Set<AbstractUnit> filteredAbstractUnits =
-          new HashSet<>(timetableSideBar.getAbstractUnitFilter().getSelectedAbstractUnits());
-
-      if (filteredAbstractUnits.isEmpty()) {
-        return false;
-      }
-
-      final Set<AbstractUnit> sessionAbstractUnits = session.getIntendedAbstractUnits();
-
-      sessionAbstractUnits.retainAll(filteredAbstractUnits);
-
-      return !filteredAbstractUnits.isEmpty() && sessionAbstractUnits.isEmpty();
-    }
-  }
-
   private class ConflictedSemestersBinding extends SetBinding<String> {
 
     private final ObservableStore store;
@@ -345,6 +262,109 @@ public class Timetable extends SplitPane implements Initializable, Activatable {
               Collectors.collectingAndThen(
                   Collectors.mapping(String::valueOf, Collectors.toSet()),
                   FXCollections::observableSet));
+    }
+  }
+
+  private class SlotBinding extends ListBinding<SessionFacade> {
+
+    private final SessionFacade.Slot slot;
+
+    SlotBinding(final SessionFacade.Slot slot) {
+      this.slot = slot;
+
+      bind(sessions);
+    }
+
+    @Override
+    protected ObservableList<SessionFacade> computeValue() {
+      return sessions.filtered(facade -> facade.getSlot().equals(slot));
+    }
+  }
+
+  private class SemesterBinding extends ListBinding<SessionFacade> {
+
+    private final ListBinding<SessionFacade> slotBinding;
+
+    SemesterBinding(final ListBinding<SessionFacade> slotBinding) {
+      this.slotBinding = slotBinding;
+      bind(slotBinding);
+      bind(semesterToggle.getToggleGroup().selectedToggleProperty());
+    }
+
+    @Override
+    protected ObservableList<SessionFacade> computeValue() {
+      final Integer semester = getSelectedSemester();
+      return slotBinding.filtered(facade -> {
+        final Set<Integer> semesters = facade.getUnitSemesters();
+        return semester == null || semesters.contains(semester);
+      });
+    }
+
+    private Integer getSelectedSemester() {
+      final ToggleButton semesterButton =
+          (ToggleButton) semesterToggle.getToggleGroup().getSelectedToggle();
+
+      if (null != semesterButton) {
+        return Integer.valueOf((String) semesterButton.getUserData());
+      }
+
+      return null;
+    }
+  }
+
+  private class FilterBinding extends ListBinding<SessionFacade> {
+    private final ListBinding<SessionFacade> semesterBinding;
+    private Set<Course> filteredCourses;
+    private Set<AbstractUnit> filteredAbstractUnits;
+
+    FilterBinding(final ListBinding<SessionFacade> semesterBinding) {
+
+      this.semesterBinding = semesterBinding;
+      bind(semesterBinding);
+      bind(timetableSideBar.getSetOfCourseSelection().selectedCoursesProperty(),
+          timetableSideBar.getAbstractUnitFilter().selectedAbstractUnitsProperty(),
+          uiDataService.conflictMarkedSessionsProperty());
+    }
+
+    @Override
+    protected ObservableList<SessionFacade> computeValue() {
+      return semesterBinding.filtered(this::isNotExcluded);
+    }
+
+    private boolean isNotExcluded(final SessionFacade session) {
+      return sessionIsIncludedByConflict(session) || sessionIsNotExcluded(session);
+    }
+
+    private boolean sessionIsNotExcluded(final SessionFacade session) {
+      return !(sessionIsExcludedByAbstractUnit(session) || sessionIsExcludedByCourse(session));
+    }
+
+    private boolean sessionIsIncludedByConflict(final SessionFacade session) {
+      this.filteredAbstractUnits =
+        new HashSet<>(timetableSideBar.getAbstractUnitFilter().getSelectedAbstractUnits());
+      this.filteredCourses =
+        new HashSet<>(timetableSideBar.getSetOfCourseSelection().getSelectedCourses());
+
+      return uiDataService.conflictMarkedSessionsProperty().stream()
+        .anyMatch(sessionId -> sessionId == session.getId());
+    }
+
+    private boolean sessionIsExcludedByCourse(final SessionFacade session) {
+      if (filteredCourses.isEmpty()) {
+        return false;
+      }
+
+      final Set<Course> sessionCourses = session.getIntendedCourses();
+      return Collections.disjoint(filteredCourses, sessionCourses);
+    }
+
+    private boolean sessionIsExcludedByAbstractUnit(final SessionFacade session) {
+      if (filteredAbstractUnits.isEmpty()) {
+        return false;
+      }
+
+      final Set<AbstractUnit> sessionAbstractUnits = session.getIntendedAbstractUnits();
+      return Collections.disjoint(filteredAbstractUnits, sessionAbstractUnits);
     }
   }
 }
