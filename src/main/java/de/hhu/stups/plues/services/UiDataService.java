e50e2b7f249d502817743feccbd8800e0fe50aa9
David Schneider <david.schneider@bivab.de>
2017-01-26 11:06:55 +0100

    Renamed accessor for sessionDisplayFormat

--- src/main/java/de/hhu/stups/plues/services/UiDataService.java
+++ src/main/java/de/hhu/stups/plues/services/UiDataService.java
@@ -97,7 +97,7 @@ public class UiDataService {
     });
   }
 
-  public String getSessionDisplayFormatProperty() {
+  public String getSessionDisplayFormat() {
     return sessionDisplayFormatProperty.get();
   }
 
