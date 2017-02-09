13cd49f2e464cac2dea351de99e32579867dd44c
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-02-09 11:50:15 +0100

    resolve warnings

--- src/main/java/de/hhu/stups/plues/ui/components/timetable/SemesterChooser.java
+++ src/main/java/de/hhu/stups/plues/ui/components/timetable/SemesterChooser.java
@@ -9,7 +9,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableSet;
-import javafx.event.EventHandler;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -38,7 +37,7 @@ public class SemesterChooser extends SegmentedButton {
     init();
   }
 
-  @SuppressWarnings("unused")
+  @SuppressWarnings({"unused","WeakerAccess"})
   public SemesterChooser(final ObservableList<ToggleButton> semesters) {
     super(semesters);
     init();
@@ -49,24 +48,22 @@ public class SemesterChooser extends SegmentedButton {
     selectedSemestersBinding = new SelectedSemestersBinding();
     selectedSemesters.bind(selectedSemestersBinding);
 
-    final EventHandler<MouseEvent> handleMouseClicked = this::handleMouseClicked;
-    final EventHandler<KeyEvent> handleKeyPressed = this::handleKeyPressed;
-
     getButtons().addListener((ListChangeListener<ToggleButton>) c -> {
       while (c.next()) {
         c.getAddedSubList().forEach(o -> {
-          o.addEventFilter(MouseEvent.MOUSE_CLICKED, handleMouseClicked);
-          o.addEventFilter(KeyEvent.KEY_PRESSED, handleKeyPressed);
+          o.addEventFilter(MouseEvent.MOUSE_CLICKED, this::handleMouseClicked);
+          o.addEventFilter(KeyEvent.KEY_PRESSED, this::handleKeyPressed);
         });
       }
       c.getRemoved().forEach(o -> {
-        o.removeEventFilter(MouseEvent.MOUSE_CLICKED, handleMouseClicked);
-        o.removeEventFilter(KeyEvent.KEY_PRESSED, handleKeyPressed);
+        o.removeEventFilter(MouseEvent.MOUSE_CLICKED, this::handleMouseClicked);
+        o.removeEventFilter(KeyEvent.KEY_PRESSED, this::handleKeyPressed);
 
       });
     });
   }
 
+  @SuppressWarnings("unused")
   private void handleMouseClicked(final MouseEvent event) {
     final ToggleButton button = (ToggleButton) event.getSource();
 
@@ -88,6 +85,7 @@ public class SemesterChooser extends SegmentedButton {
     selectedSemesters.bind(selectedSemestersBinding);
   }
 
+  @SuppressWarnings("unused")
   private void handleKeyPressed(final KeyEvent event) {
     if (event.getCode() != KeyCode.SPACE) {
       return;
