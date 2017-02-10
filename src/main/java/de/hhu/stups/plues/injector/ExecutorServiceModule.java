3a41ad1f7afffbe6b94e1a321f4cedeb32161ef2
David Schneider <david.schneider@bivab.de>
2017-02-10 12:54:29 +0100

    Removed unused import

--- src/main/java/de/hhu/stups/plues/injector/ExecutorServiceModule.java
+++ src/main/java/de/hhu/stups/plues/injector/ExecutorServiceModule.java
@@ -3,7 +3,6 @@ package de.hhu.stups.plues.injector;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.inject.AbstractModule;
-import com.google.inject.name.Names;
 
 import de.hhu.stups.plues.tasks.ObservableListeningExecutorService;
 
