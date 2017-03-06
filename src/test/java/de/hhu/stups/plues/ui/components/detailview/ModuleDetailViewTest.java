eacabcbfba07b74844983b8bfcbddd8b61dddf40
David Schneider <david.schneider@bivab.de>
2017-03-06 15:36:08 +0100

    Updated ModuleDetailView
    * Mandatory and Level are not part of the module
    * Show a table of moduleLevel entries instead of only the courses
    
    References #199 #200

--- src/test/java/de/hhu/stups/plues/ui/components/detailview/ModuleDetailViewTest.java
+++ src/test/java/de/hhu/stups/plues/ui/components/detailview/ModuleDetailViewTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.mock;
 import de.hhu.stups.plues.data.entities.AbstractUnit;
 import de.hhu.stups.plues.data.entities.Course;
 import de.hhu.stups.plues.data.entities.Module;
+import de.hhu.stups.plues.data.entities.ModuleLevel;
 import de.hhu.stups.plues.routes.Router;
 import de.hhu.stups.plues.ui.layout.Inflater;
 import de.hhu.stups.plues.ui.layout.SceneFactory;
@@ -31,12 +32,12 @@ public class ModuleDetailViewTest extends ApplicationTest {
 
   private static final String TITLE = "Module 1";
   private static final int PORDNR = 1;
-  private static final String NAME = "Mod";
   private static final String MANDATORY = "✗";
   private static final int CREDIT_POINTS = 100;
   private static final int ELECTIVE_UNITS = 0;
   private final List<Course> courses;
   private final List<AbstractUnit> abstractUnits;
+  private final List<ModuleLevel> moduleLevels;
   private Module module;
 
   /**
@@ -52,6 +53,7 @@ public class ModuleDetailViewTest extends ApplicationTest {
 
     this.courses = Arrays.asList(course1, course2);
     this.abstractUnits = Arrays.asList(au1, au2);
+    this.moduleLevels = Arrays.asList(new ModuleLevel());
 
     doReturn("Course 1").when(course1).getName();
     doReturn("BK-Course-1").when(course1).getFullName();
@@ -65,12 +67,10 @@ public class ModuleDetailViewTest extends ApplicationTest {
 
     doReturn(new HashSet<>(courses)).when(module).getCourses();
     doReturn(new HashSet<>(abstractUnits)).when(module).getAbstractUnits();
+    doReturn(new HashSet<>(moduleLevels)).when(module).getModuleLevels();
 
     doReturn(TITLE).when(module).getTitle();
     doReturn(PORDNR).when(module).getPordnr();
-    doReturn(NAME).when(module).getName();
-    doReturn(false).when(module).getMandatory();
-    doReturn(CREDIT_POINTS).when(module).getCreditPoints();
     doReturn(ELECTIVE_UNITS).when(module).getElectiveUnits();
   }
 
@@ -82,32 +82,24 @@ public class ModuleDetailViewTest extends ApplicationTest {
     final Label titleLabel = lookup("#title").query();
     Assert.assertEquals(TITLE, titleLabel.getText());
 
-    final Label nameLabel = lookup("#name").query();
-    Assert.assertEquals(NAME, nameLabel.getText());
-
-    final Label mandatoryLabel = lookup("#mandatory").query();
-    Assert.assertEquals(MANDATORY, mandatoryLabel.getText());
-
-    final Label creditPointsLabel = lookup("#creditPoints").query();
-    Assert.assertEquals(CREDIT_POINTS, Integer.parseInt(creditPointsLabel.getText()));
-
     final Label electiveUnitsLabel = lookup("#electiveUnits").query();
     Assert.assertEquals(ELECTIVE_UNITS, Integer.parseInt(electiveUnitsLabel.getText()));
   }
 
   @Test
   public void testContentSize() {
-    final TableView<Course> courseTable = lookup("#courseTableView").query();
+    final TableView<Course> courseTable = lookup("#moduleLevelTableView").query();
     final TableView<AbstractUnit> abstractUnitTableView = lookup("#abstractUnitTableView").query();
 
-    Assert.assertEquals(courses.size(), courseTable.getItems().size());
+    Assert.assertEquals(moduleLevels.size(), courseTable.getItems().size());
     Assert.assertEquals(abstractUnits.size(), abstractUnitTableView.getItems().size());
   }
 
   @Test
   public void testCourseTableContent() {
-    final TableView<Course> courseTable = lookup("#courseTableView").query();
-    courses.forEach(course -> Assert.assertThat(courseTable.getItems(), hasItem(course)));
+    final TableView<ModuleLevel> moduleLevelTableView = lookup("#moduleLevelTableView").query();
+    moduleLevels.forEach(moduleLevel
+        -> Assert.assertThat(moduleLevelTableView.getItems(), hasItem(moduleLevel)));
   }
 
   @Test
