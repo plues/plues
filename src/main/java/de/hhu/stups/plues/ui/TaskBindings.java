329f7937d7bba83ebdb8fe830f418cf4ac37138e
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-03-15 18:51:15 +0100

    center icons

--- src/main/java/de/hhu/stups/plues/ui/TaskBindings.java
+++ src/main/java/de/hhu/stups/plues/ui/TaskBindings.java
@@ -2,6 +2,7 @@ package de.hhu.stups.plues.ui;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
+
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.binding.StringBinding;
@@ -81,11 +82,11 @@ public class TaskBindings {
   public static StringBinding getStyleBinding(final Task<?> task) {
     return Bindings.createStringBinding(() -> {
       final String color = getColor(task);
-
+      final String centerIcon = "-fx-alignment: center;";
       if (color == null) {
-        return "";
+        return centerIcon;
       }
-      return "-fx-background-color: " + color;
+      return "-fx-background-color: " + color + "; " + centerIcon;
     }, task.stateProperty());
   }
 
