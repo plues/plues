b48768d46feb28b1333afd98d0f0a18853b575dd
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-03-29 18:53:19 +0200

    add test for the CheckCourseFeasibility component

---
+++ src/test/java/de/hhu/stups/plues/ui/components/CheckCourseFeasibilityTest.java
@@ -0,0 +1,190 @@
+package de.hhu.stups.plues.ui.components;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testfx.api.FxToolkit.setupStage;
+
+import de.hhu.stups.plues.Delayed;
+import de.hhu.stups.plues.data.Store;
+import de.hhu.stups.plues.data.entities.Course;
+import de.hhu.stups.plues.routes.Router;
+import de.hhu.stups.plues.services.SolverService;
+import de.hhu.stups.plues.services.UiDataService;
+import de.hhu.stups.plues.ui.layout.Inflater;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+import org.junit.After;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class CheckCourseFeasibilityTest extends ApplicationTest {
+
+  private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+  private final ObservableList<Course> courseList = FXCollections.observableArrayList();
+  private final Store store;
+
+  private CheckCourseFeasibility checkCourseFeasibility;
+  private CombinationOrSingleCourseSelection courseSelection;
+  private ListView<FeasibilityBox> feasibilityBoxWrapper;
+
+  public CheckCourseFeasibilityTest() {
+    store = mock(Store.class);
+  }
+
+  @Test
+  public void testCheckFeasibilityComputation() {
+    clickOn(courseSelection.getMajorMinorCourseSelection().getMajorComboBox())
+        .type(KeyCode.DOWN)
+        .type(KeyCode.ENTER);
+    assertFalse(feasibilityBoxWrapper.isVisible());
+    assertEquals(0, feasibilityBoxWrapper.getItems().size());
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(1, feasibilityBoxWrapper.getItems().size());
+    assertTrue(feasibilityBoxWrapper.isVisible());
+
+    clickOn(courseSelection.getMajorMinorCourseSelection().getMajorComboBox())
+        .type(KeyCode.DOWN)
+        .type(KeyCode.ENTER);
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(2, feasibilityBoxWrapper.getItems().size());
+  }
+
+  @Test
+  public void testNoRedundantFeasibilityBoxes() {
+    clickOn(courseSelection.getMajorMinorCourseSelection().getMajorComboBox())
+        .type(KeyCode.DOWN)
+        .type(KeyCode.ENTER);
+    assertFalse(feasibilityBoxWrapper.isVisible());
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(1, feasibilityBoxWrapper.getItems().size());
+    assertTrue(feasibilityBoxWrapper.isVisible());
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(1, feasibilityBoxWrapper.getItems().size());
+    assertTrue(feasibilityBoxWrapper.isVisible());
+
+    clickOn(courseSelection.getMajorMinorCourseSelection().getMajorComboBox())
+        .type(KeyCode.UP)
+        .type(KeyCode.ENTER);
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(2, feasibilityBoxWrapper.getItems().size());
+    assertTrue(feasibilityBoxWrapper.isVisible());
+  }
+
+  @Test
+  public void testExistingFeasibilityBoxToTop() {
+    assertFalse(feasibilityBoxWrapper.isVisible());
+    checkCourseFeasibility.checkFeasibility();
+    clickOn(courseSelection.getMajorMinorCourseSelection().getMajorComboBox())
+        .type(KeyCode.DOWN)
+        .type(KeyCode.ENTER);
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(2, feasibilityBoxWrapper.getItems().size());
+    assertTrue(feasibilityBoxWrapper.isVisible());
+    final FeasibilityBox existingFeasibilityBox = feasibilityBoxWrapper.getItems().get(1);
+    clickOn(courseSelection.getMajorMinorCourseSelection().getMajorComboBox())
+        .type(KeyCode.UP)
+        .type(KeyCode.ENTER);
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(existingFeasibilityBox, feasibilityBoxWrapper.getItems().get(0));
+    clickOn(courseSelection.getMajorMinorCourseSelection().getMajorComboBox())
+        .type(KeyCode.DOWN)
+        .type(KeyCode.ENTER);
+    checkCourseFeasibility.checkFeasibility();
+    sleep(1, TimeUnit.SECONDS);
+    assertEquals(existingFeasibilityBox, feasibilityBoxWrapper.getItems().get(1));
+  }
+
+  @After
+  public void cleanup() throws Exception {
+    WaitForAsyncUtils.waitForFxEvents();
+    setupStage(Stage::close);
+  }
+
+  @Override
+  public void start(final Stage stage) throws Exception {
+    courseList.add(UiTestHelper.createCourse("shortName1", "bk", "H"));
+    courseList.add(UiTestHelper.createCourse("shortName2", "ba", "H"));
+    courseList.add(UiTestHelper.createCourse("shortName3", "bk", "N"));
+    courseList.add(UiTestHelper.createCourse("shortName4", "ma", "N"));
+    courseList.add(UiTestHelper.createCourse("shortName5", "ma", "N"));
+    courseList.add(UiTestHelper.createCourse("shortName6", "bk", "H"));
+    courseList.add(UiTestHelper.createCourse("shortName7", "ma", "H"));
+
+    final FXMLLoader loader = new FXMLLoader();
+    loader.setBuilderFactory(type -> {
+      if (type.equals(MajorMinorCourseSelection.class)) {
+        return () -> new MajorMinorCourseSelection(new Inflater(new FXMLLoader()));
+      } else if (type.equals(CombinationOrSingleCourseSelection.class)) {
+        return () -> courseSelection;
+      } else if (type.equals(TaskProgressIndicator.class)) {
+        return () -> new TaskProgressIndicator(new Inflater(new FXMLLoader()));
+      }
+      return new JavaFXBuilderFactory().getBuilder(type);
+    });
+
+    final Inflater inflater = new Inflater(loader);
+    final SolverService solverService = mock(SolverService.class);
+    when(solverService.checkFeasibilityTask(any()))
+        .thenReturn(UiTestHelper.getSimpleCheckFeasibilityTask());
+    when(solverService.checkFeasibilityTask(any(), any()))
+        .thenReturn(UiTestHelper.getSimpleCheckFeasibilityTask());
+    when(solverService.impossibleCoursesTask())
+        .thenReturn(UiTestHelper.getSimpleImpossibleCoursesTask());
+
+    final Delayed<SolverService> delayedSolverService = new Delayed<>();
+    delayedSolverService.set(solverService);
+    courseSelection = new CombinationOrSingleCourseSelection(inflater);
+    courseSelection.setCourses(courseList);
+
+    final Delayed<Store> delayedStore = new Delayed<>();
+    when(store.getCourses()).thenReturn(courseList);
+    delayedStore.set(store);
+
+    final UiDataService uiDataService = new UiDataService(delayedSolverService, delayedStore,
+        executorService);
+
+    final Router router = new Router();
+
+    final FeasibilityBoxFactory feasibilityBoxFactory = mock(FeasibilityBoxFactory.class);
+    when(feasibilityBoxFactory.create(any(), any(), any()))
+        .thenAnswer(invocation ->
+            new FeasibilityBox(inflater, router, delayedSolverService, executorService,
+                () -> null, uiDataService,
+                courseSelection.getMajorMinorCourseSelection().getSelectedMajor(),
+                courseSelection.getMajorMinorCourseSelection().getSelectedMinor(),
+                feasibilityBoxWrapper));
+
+    checkCourseFeasibility = new CheckCourseFeasibility(inflater, feasibilityBoxFactory,
+        delayedSolverService, uiDataService);
+    feasibilityBoxWrapper = checkCourseFeasibility.getFeasibilityBoxWrapper();
+
+    final Scene scene = new Scene(checkCourseFeasibility, 400, 500);
+
+    stage.setScene(scene);
+    stage.show();
+  }
+}
