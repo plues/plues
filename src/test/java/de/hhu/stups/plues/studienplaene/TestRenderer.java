80ed5c2a6405b0b65dbe92bf75cf660adfd218d4
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-04-18 15:32:38 +0200

    adapt test #215

--- src/test/java/de/hhu/stups/plues/studienplaene/TestRenderer.java
+++ src/test/java/de/hhu/stups/plues/studienplaene/TestRenderer.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashSet;
 
 
 public class TestRenderer extends TestBase {
@@ -25,7 +27,13 @@ public class TestRenderer extends TestBase {
 
   @Test
   public void testItWorksForColor() throws RenderingException, IOException {
-    final Renderer renderer = new Renderer(store, result, course, ColorChoice.COLOR);
+    final ColorScheme colorScheme = new ColorScheme("Something",
+        new HashSet<>(Arrays.asList("#DCBFBE", "#DCD6BE", "#C1DCBE", "#F1EAB4", "#C5CBF1",
+            "#EFF1CB", "#E5CBF1", "#DCF1E9", "#EFB9B9", "#FFA6A6", "#FCFE80", "#C7FF72",
+            "#9AFFA4", "#9AFFD6", "#9AFFF9", "#94E5FF", "#A4C1FF", "#CFA4FF", "#F2A4FF",
+            "#F6CCFF", "#FFB5F0", "#F7D9E4", "#E78FFB", "#DAFFB4", "#B4FFFD", "#69BCFF",
+            "#FFA361")));
+    final Renderer renderer = new Renderer(store, result, course, colorScheme);
     final ByteArrayOutputStream result = renderer.getResult();
 
     final File pdf = File.createTempFile("color", ".pdf");
@@ -37,7 +45,8 @@ public class TestRenderer extends TestBase {
 
   @Test
   public void testItWorksForGrayscale() throws RenderingException, IOException {
-    final Renderer renderer = new Renderer(store, result, course, ColorChoice.GRAYSCALE);
+    final ColorScheme colorScheme = new ColorScheme("grayscale", new HashSet<>());
+    final Renderer renderer = new Renderer(store, result, course, colorScheme);
     final ByteArrayOutputStream result = renderer.getResult();
 
     final File pdf = File.createTempFile("gray", ".pdf");
