4e014d5ba66d4e96068d03c36f0a34141327e2e7
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-06-13 11:48:37 +0200

    Add Majors and Minors to CourseDetailView, closes #243

--- src/main/java/de/hhu/stups/plues/ui/components/detailview/CourseDetailView.java
+++ src/main/java/de/hhu/stups/plues/ui/components/detailview/CourseDetailView.java
@@ -3,14 +3,21 @@ package de.hhu.stups.plues.ui.components.detailview;
 import com.google.inject.Inject;
 
 import de.hhu.stups.plues.data.entities.Course;
+import de.hhu.stups.plues.routes.RouteNames;
+import de.hhu.stups.plues.routes.Router;
 import de.hhu.stups.plues.ui.layout.Inflater;
+
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -20,6 +27,9 @@ import java.util.ResourceBundle;
 public class CourseDetailView extends VBox implements Initializable, DetailView {
 
   private final ObjectProperty<Course> courseProperty;
+  private final Router router;
+
+  private ResourceBundle resources;
 
   @FXML
   @SuppressWarnings("unused")
@@ -36,6 +46,18 @@ public class CourseDetailView extends VBox implements Initializable, DetailView 
   @FXML
   @SuppressWarnings("unused")
   private Label degree;
+  @FXML
+  @SuppressWarnings("unused")
+  private Label lbMajorOrMinorCourses;
+  @FXML
+  @SuppressWarnings("unused")
+  private TableView<Course> tableViewMajorOrMinorCourses;
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Course, String> tableColumnCourseKey;
+  @FXML
+  @SuppressWarnings("unused")
+  private TableColumn<Course, String> tableColumnCourseName;
 
   @FXML
   @SuppressWarnings("unused")
@@ -45,7 +67,9 @@ public class CourseDetailView extends VBox implements Initializable, DetailView 
    * Default constructor.
    */
   @Inject
-  public CourseDetailView(final Inflater inflater) {
+  public CourseDetailView(final Inflater inflater,
+                          final Router router) {
+    this.router = router;
     courseProperty = new SimpleObjectProperty<>();
 
     inflater.inflate("components/detailview/CourseDetailView", this, this, "detailView", "Column");
@@ -61,6 +85,7 @@ public class CourseDetailView extends VBox implements Initializable, DetailView 
 
   @Override
   public void initialize(final URL location, final ResourceBundle resources) {
+    this.resources = resources;
     this.key.textProperty().bind(Bindings.when(courseProperty.isNotNull()).then(
         Bindings.selectString(courseProperty, "key")).otherwise(""));
 
@@ -79,5 +104,29 @@ public class CourseDetailView extends VBox implements Initializable, DetailView 
     final IntegerBinding cp = Bindings.selectInteger(courseProperty, "creditPoints");
     this.creditPoints.textProperty().bind(
         Bindings.when(cp.greaterThan(0)).then(cp.asString()).otherwise("-"));
+
+    courseProperty.addListener((observable, oldValue, newValue) -> initializeTableView());
+  }
+
+  private void initializeTableView() {
+    tableColumnCourseKey.setCellValueFactory(value ->
+        new ReadOnlyStringWrapper(value.getValue().getKey()));
+    tableColumnCourseName.setCellValueFactory(value ->
+        new ReadOnlyStringWrapper(value.getValue().getFullName()));
+    tableViewMajorOrMinorCourses.setOnMouseClicked(this::tableViewMouseClicked);
+    // add major or minor courses according to the current course
+    final Course currentCourse = courseProperty.get();
+    tableViewMajorOrMinorCourses.getItems().addAll(currentCourse.isMajor()
+        ? currentCourse.getMinorCourses() : currentCourse.getMajorCourses());
+    lbMajorOrMinorCourses.setText(courseProperty.get().isMajor()
+        ? resources.getString("minorCourses") : resources.getString("majorCourses"));
+  }
+
+  private void tableViewMouseClicked(final MouseEvent mouseEvent) {
+    if (mouseEvent.getClickCount() < 2) {
+      return;
+    }
+    router.transitionTo(RouteNames.COURSE_DETAIL_VIEW,
+        tableViewMajorOrMinorCourses.getSelectionModel().getSelectedItem());
   }
 }
