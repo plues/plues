e0a98c2652779a73db2f6eb31b64dbb0490c5e47
David Schneider <david.schneider@bivab.de>
2017-08-01 18:29:19 +0200

    Upgraded ProB Java library to 3.2.2
    Upgraded gradle file to reference version 3.2.2 of the ProB 2 Java
    library and fixed a (seemingly new) ConcurrentModificationException
    in the code that unsubscribes from all the formulas in the state.
    
    Closes #249

--- src/main/java/de/hhu/stups/plues/prob/ProBSolver.java
+++ src/main/java/de/hhu/stups/plues/prob/ProBSolver.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -62,8 +63,9 @@ public class ProBSolver implements Solver {
     final long t2 = System.nanoTime();
     logger.info("Loaded machine in {} ms.", TimeUnit.NANOSECONDS.toMillis(t2 - t1));
     //
-    this.stateSpace.getSubscribedFormulas()
+    new HashSet<>(this.stateSpace.getSubscribedFormulas())
         .forEach(it -> stateSpace.unsubscribe(this.stateSpace, it));
+
 
     final long t3 = System.nanoTime();
     this.trace = traceFrom(stateSpace);
